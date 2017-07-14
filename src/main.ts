4de4a82fe85fafe077a4657d516c978656ffb434
Deomitrus <github@deomitrus.com>
2017-07-13 20:52:54 -0400

    reorg

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 process.title = 'veonim'
 
-import { attach } from './neovim'
-import './render'
+import { attach } from './core/neovim'
+import './core/render'
 
 attach(100, 80)
 
 // async plugin loading after (hopefully) initial render pass
-setImmediate(() => require('./plugins'))
+// TODO: soon!
+//setImmediate(() => require('./plugins'))
