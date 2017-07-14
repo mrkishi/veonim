f119b87f680f5122cff9c3594e0e22fa89e933fc
Deomitrus <github@deomitrus.com>
2017-07-13 23:35:32 -0400

    devmode

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { app, BrowserWindow } from 'electron'
-const DEBUG = process.argv.includes('--dev')
+const DEVMODE = process.argv.includes('--dev')
 
 let win
 app.setName('veonim')
 app.on('ready', () => {
   win = new BrowserWindow({ width: 1200, height: 800 })
   win.loadURL(`file:///${__dirname}/index.html`)
-  DEBUG && win.webContents.openDevTools()
+  DEVMODE && win.webContents.openDevTools()
 })
