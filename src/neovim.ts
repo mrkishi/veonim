c32222d7bf2eb08b1cc39819bdeb1675141c3b1d
Deomitrus <github@deomitrus.com>
2017-07-18 19:46:21 -0400

    add ex, expr, and get/set variable to neovim api

--- src/neovim.ts
+++ src/neovim.ts
@@ -12,10 +12,22 @@ const baseAttachOpts = {
   ext_cmdline: false
 }
 
+type VimVariable = { [key: string]: any }
+
+export const g = new Proxy({} as VimVariable, {
+  get: async (_, key) => {
+    const val = await req.getVar(key as string).catch(e => e)
+    if (!Array.isArray(val) && val[1] !== 'Key not found') return val
+  },
+  set: (_, key, val) => (api.setVar(key as string, val), true)
+})
+
 export const attach = (width: number, height: number, opts = baseAttachOpts) => api.uiAttach(width, height, { ...baseAttachOpts, ...opts })
 export const resize = (width: number, height: number) => api.uiTryResize(width, height)
 export const input = (m: string) => api.input(m)
 export const cmd = (m: string) => api.command(m)
+export const ex = (m: string) => req.commandOutput(m)
+export const expr = (m: string) => req.eval(m)
 export const buffers = () => req.listBufs()
 export const action = (event: string, fn: Function) => watchers.add(event, fn)
 export const call: Functions = onFnCall((name: string, args: any[] = []) => req.callFunction(name, args))
