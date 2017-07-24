136ad78291f5ef49bd594564b527eca07cb5e82e
Deomitrus <github@deomitrus.com>
2017-07-24 17:10:08 -0400

    config reader lul

---
+++ src/config-reader.ts
@@ -0,0 +1,28 @@
+import { homedir } from 'os'
+import { createReadStream, stat } from 'fs'
+import { promisify } from 'util'
+
+const exists = promisify(stat)
+const $HOME = homedir()
+// /^(?:"|')(.*)(?:"|')$/.replace($1)
+
+const possibleLocations = [
+  `${$HOME}/.config/nvim/init.vim`,
+  `${$HOME}/.vimrc`,
+]
+// TODO: chokidar
+
+export default async () => {
+  // TODO: check if file exists
+  const checks = await Promise.all(possibleLocations.map(p => exists(p)))
+  const file = checks.find(a => a)
+  const f = createReadStream(file)
+  f.on('data', (m: Buffer) => {
+    const config = m
+      .toString()
+      .split('\n')
+      .filter(line => line.match(''))
+
+    return config
+  })
+}
