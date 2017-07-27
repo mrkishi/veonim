81e1dd6e09c90562e801ac86911e08cfa800314a
goavn <goavn@deomitrus.com>
2017-07-27 00:50:38 -0400

    fix and cleanup type of information

--- src/utils.ts
+++ src/utils.ts
@@ -28,28 +28,14 @@ interface Types {
 }
 
 export const cc = (...a: any[]) => Promise.all(a)
-export const $ = (...fns: Function[]) => (...a: any[]) => fns.reduce((res, fn, ix) => ix ? fn(res) : fn(...res), a)
-
-export const type = (m: any) => {
-  const [ , type ] = Object.prototype.toString.call(m).match(/^\[object (\w+)\]/g)
-  return (type || 'undefined').toLowerCase()
-}
-
 export const ID = (val = 0) => ({ next: () => (val++, val) })
-
+export const $ = (...fns: Function[]) => (...a: any[]) => fns.reduce((res, fn, ix) => ix ? fn(res) : fn(...res), a)
+export const type = (m :any) => (Object.prototype.toString.call(m).match(/^\[object (\w+)\]/) || [])[1].toLowerCase()
 export const is = new Proxy<Types>({} as Types, { get: (_, key) => (val: any) => type(val) === key })
-
 export const onProp = <T>(cb: (name: PropertyKey) => void): T => new Proxy({}, { get: (_, name) => cb(name) }) as T
 export const onFnCall = <T>(cb: (name: string, args: any[]) => void): T => new Proxy({}, { get: (_, name) => (...args: any[]) => cb(name as string, args) }) as T
-
 export const pascalCase = (m: string) => m[0].toUpperCase() + m.slice(1)
 export const snakeCase = (m: string) => m.split('').map(ch => /[A-Z]/.test(ch) ? '_' + ch.toLowerCase(): ch).join('')
-
-export const promisifyApi = <T>(o: object): T => onFnCall<T>((name: string) => (...args: any[]) => new Promise((ok, no) => {
-  const theFunctionToCall: Function = Reflect.get(o, name)
-  theFunctionToCall(...args, (err: Error, res: any) => err ? no(err) : ok(res))
-}))
-
 export const merge = (target, source) => Object.entries(source).reduce((tar, [k, v]) => (v && Reflect.set(tar, k, v), tar), target)
 
 export function debounce (fn: Function, wait = 1) {
