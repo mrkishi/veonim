b1d8fa881fc7f3a2f0c9f45287e7393e8ce85be6
Deomitrus <github@deomitrus.com>
2017-07-29 17:07:12 -0400

    add define vim function capability - #20

--- src/ui/neovim-client.ts
+++ src/ui/neovim-client.ts
@@ -1,6 +1,6 @@
 import { Neovim } from '../neovim'
 import Channel from '../channel'
-import { onFnCall } from '../utils'
+import { onFnCall, pascalCase } from '../utils'
 import { Functions } from '../functions'
 
 const io = new Worker(`${__dirname}/../workers/io.js`)
@@ -11,3 +11,15 @@ export { on, sub }
 export const notify = Notifier<Neovim>()
 export const request = Requester<Neovim>()
 export const call: Functions = onFnCall((name, args) => request.call(name, args))
+
+const { cmd } = notify
+
+export const define: { [index: string]: (fnBody: string) => void } = onFnCall((name: string, [ fn ]: string[]) => {
+  const expr = fn[0]
+    .split('\n')
+    .filter(m => m)
+    .join('\\n')
+    .replace(/"/g, '\\"')
+
+  cmd(`exe ":fun! ${pascalCase(name)}(...) range\n${expr}\nendfun"`)
+})
