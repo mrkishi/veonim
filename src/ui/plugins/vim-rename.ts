bc16ec8615bdd7194d9aa67d60a4e3e37b2e38da
Deomitrus <github@deomitrus.com>
2017-08-02 19:44:50 -0400

    session val instead of placeholder - fix #55

--- src/ui/plugins/vim-rename.ts
+++ src/ui/plugins/vim-rename.ts
@@ -3,20 +3,20 @@ import { renameCurrent, getCurrentName } from '../sessions'
 import { h, app } from './plugins'
 import TermInput from './input'
 
-interface State { val: string, vis: boolean, current: string }
-const state: State = { val: '', vis: false, current: '' }
+interface State { val: string, vis: boolean }
+const state: State = { val: '', vis: false }
 
-const view = ({ val, vis, current }: State, { select, hide, change }: any) => h('#vim-rename.plugin', {
+const view = ({ val, vis }: State, { select, hide, change }: any) => h('#vim-rename.plugin', {
   hide: !vis
 }, [
   h('.dialog.small', [
-    TermInput({ focus: true, desc: current, val, select, hide, change }),
+    TermInput({ focus: true, val, select, hide, change }),
   ])
 ])
 
 const a: Actions<State> = {}
 
-a.show = (_s, _a, current: string) => ({ current, vis: true }),
+a.show = (_s, _a, val: string) => ({ val, vis: true }),
 a.hide = () => ({ val: '', vis: false })
 a.change = (_s, _a, val: string) => ({ val })
 a.select = (s, a) => {
