8f6f885cbb84dcaa64393b7ac8d1a3920d55c992
Deomitrus <github@deomitrus.com>
2017-08-02 22:38:37 -0400

    begin work on input key transforms - #23

--- src/ui/input.ts
+++ src/ui/input.ts
@@ -5,6 +5,7 @@ const { input } = notify
 const modifiers = ['Alt', 'Shift', 'Meta', 'Control']
 const remaps = new Map<string, string>()
 let isCapturing = false
+let down: string
 
 const isStandardAscii = (key: string) => key.charCodeAt(0) > 32 && key.charCodeAt(0) < 127
 const handleMods = ({ ctrlKey, shiftKey, metaKey, altKey, key }: KeyboardEvent) => {
@@ -34,7 +35,7 @@ const toVimKey = (key: string): string => {
 }
 
 const bypassEmptyMod = (key: string) => modifiers.includes(key) ? '' : key
-const wrapKey = (key: string): string => key.length > 1 ? `<${key}>` : key
+const wrapKey = (key: string): string => key.length > 1 && key[0].toUpperCase() === key[0] ? `<${key}>` : key
 const combineModsWithKey = (mods: string, key: string) => mods.length ? `${mods}-${key}` : key
 const userModRemaps = (mods: string[]) => mods.map(m => remaps.get(m) || m)
 const joinModsWithDash = (mods: string[]) => mods.join('-')
@@ -48,14 +49,48 @@ export const focus = () => isCapturing = true
 export const blur = () => isCapturing = false
 export const registerShortcut = (keys: string, cb: Function) => shortcuts.set(`<${keys.toUpperCase()}>`, cb)
 
+type Transformer = (input: KeyboardEvent) => KeyboardEvent
+const xforms = new Map<string, Transformer>()
+
+const keToStr = (e: KeyboardEvent) => [e.key, <any>e.ctrlKey|0, <any>e.metaKey|0, <any>e.altKey|0, <any>e.shiftKey|0].filter(a => a).join('')
+
+export const addTransformer = (e: KeyboardEvent, fn: Transformer) => xforms.set(keToStr(e), fn)
+
+const defkey = {...new KeyboardEvent('keydown'), key: '', ctrlKey: false, metaKey: false, altKey: false, shiftKey: false}
+addTransformer({...defkey, key: ';'}, e => ({...e, key: ';' + e.key}))
+// TODO: add transform for alone (down + up => new key) aka ctrl/esc
+
+console.log(xforms)
+
 window.addEventListener('keydown', e => {
   if (!isCapturing) return
-  const key = bypassEmptyMod(e.key)
+  const strKey = keToStr(e)
+
+  console.log('strkey', strKey)
+  console.log('down', down)
+  if (xforms.has(down)) console.log(xforms.get(down)!(e))
+
+  if (xforms.has(strKey) && down === strKey) return
+
+  const ev = xforms.has(down) ? xforms.get(down)!(e) : e
+  if (!xforms.has(down)) down = keToStr(e)
+
+  const key = bypassEmptyMod(ev.key)
   if (!key) return
 
-  const inputKeys = formatInput(mapMods(e), mapKey(e.key))
+  const inputKeys = formatInput(mapMods(ev), mapKey(ev.key))
   if (shortcuts.has(inputKeys)) return shortcuts.get(inputKeys)!()
 
+    // TODO: xform ; to ;s ;n ;e (as two chars) doesnt work because it gets wrapped in <>
+    // refactor key.length > 1 logic to not trigger on transforms. maybe look at length and first char uppercase?
+    console.log(inputKeys)
   e.preventDefault()
   input(inputKeys)
+})
+
+// no keyup events will be triggered for input value - slow anyways/vim doesn't care
+window.addEventListener('keyup', e => {
+  if (!isCapturing) return
+  e.preventDefault()
+  if (down === keToStr(e)) down = ''
 })
