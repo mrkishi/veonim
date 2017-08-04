3780926daf0dea3980198e756e883194598e9639
Deomitrus <github@deomitrus.com>
2017-08-03 21:35:26 -0400

    lousy super hacky buggy hey-holds working (still better than ke tho) - fix #23

--- src/ui/input.ts
+++ src/ui/input.ts
@@ -5,7 +5,7 @@ const { input } = notify
 const modifiers = ['Alt', 'Shift', 'Meta', 'Control']
 const remaps = new Map<string, string>()
 let isCapturing = false
-let down: string
+let holding: string
 
 const isStandardAscii = (key: string) => key.charCodeAt(0) > 32 && key.charCodeAt(0) < 127
 const handleMods = ({ ctrlKey, shiftKey, metaKey, altKey, key }: KeyboardEvent) => {
@@ -54,45 +54,73 @@ type Transformer = (input: KeyboardEvent) => KeyboardEvent
 const xforms = new Map<string, Transformer>()
 const upforms = new Map<string, Transformer>()
 
-const keToStr = (e: KeyboardEvent) => [e.key, <any>e.ctrlKey|0, <any>e.metaKey|0, <any>e.altKey|0, <any>e.shiftKey|0].filter(a => a).join('')
+const keToStr = (e: KeyboardEvent) => [e.key, <any>e.ctrlKey|0, <any>e.metaKey|0, <any>e.altKey|0, <any>e.shiftKey|0].join('')
 
 export const addTransformerDown = (e: KeyboardEvent, fn: Transformer) => xforms.set(keToStr(e), fn)
 export const addTransformUp = (e: KeyboardEvent, fn: Transformer) => upforms.set(keToStr(e), fn)
 
 const defkey = {...new KeyboardEvent('keydown'), key: '', ctrlKey: false, metaKey: false, altKey: false, shiftKey: false}
-addTransformerDown({...defkey, key: `'`}, e => ({...e, key: '@' + e.key}))
-addTransformUp({...defkey, key: `'`}, e => ({...e, key: 'Escape'}))
+addTransformerDown({...defkey, key: `;`}, e => ({...e, key: ';' + e.key}))
+// addTransformUp({...defkey, key: 'Meta'}, e => ({...e, key: 'Escape'}))
+
+let transforming = false
+
+const sendKeys = (e: KeyboardEvent) => {
+  const inputKeys = formatInput(mapMods(e), mapKey(e.key))
+  if (shortcuts.has(inputKeys)) return shortcuts.get(inputKeys)!()
+  input(inputKeys)
+}
 
 window.addEventListener('keydown', e => {
   e.preventDefault()
   if (!isCapturing) return
-
   const strKey = keToStr(e)
 
-  if (xforms.has(strKey) && down === strKey) return
+  console.log('down', strKey)
+  if (xforms.has(strKey)) {
+    holding = strKey
+    return
+  }
 
-  const ev = xforms.has(down) ? xforms.get(down)!(e) : e
-  if (!xforms.has(down)) down = keToStr(e)
+  if (!xforms.has(holding)) {
+    holding = strKey
+    transforming = false
+  }
+  else transforming = true
 
+  const ev = xforms.has(holding) ? xforms.get(holding)!(e) : e
   const key = bypassEmptyMod(ev.key)
   if (!key) return
 
-  const inputKeys = formatInput(mapMods(ev), mapKey(ev.key))
-  if (shortcuts.has(inputKeys)) return shortcuts.get(inputKeys)!()
-
-  input(inputKeys)
+  sendKeys(ev)
 })
 
 window.addEventListener('keyup', e => {
   e.preventDefault()
   if (!isCapturing) return
+  const strKey = keToStr(e)
+  
+  // TODO: Lol this is maximum dirty hack
+  // TODO: after hold + transform, need to release so we can type again
+  // aka xform on ; -> ; + key
+  // then this happens
+  // <alone>;;;;<holding>;s;d;s<let go><TRY-alone-BUT-FAIL>;
+  // only if type another char that is not ; then it resets back
+  if (keToStr(e) === 'Meta0000' && holding === 'Meta0100') return input(`<Esc>`)
 
-  if (down === keToStr(e)) {
-    if (upforms.has(down)) {
-      const ev = upforms.get(down)!(e)
-      const inputKeys = formatInput(mapMods(ev), mapKey(ev.key))
-      input(inputKeys)
+  if (holding === strKey) {
+    if (xforms.has(holding) && !transforming) sendKeys(e)
+
+    else if (upforms.has(holding)) {
+      const ev = upforms.get(holding)!(e)
+      sendKeys(ev)
     }
-    down = ''
+
+    holding = ''
   }
+
+  // else if (upforms.has(strKey)) {
+  //   const ev = upforms.get(strKey)!(e)
+  //   sendKeys(ev)
+  // }
 })
