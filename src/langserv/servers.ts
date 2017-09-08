35d7ea1aa99a97638a8745665deb7422f1436a8e
Deomitrus <github@deomitrus.com>
2017-09-07 22:04:08 -0400

    setup servers

--- src/langserv/servers.ts
+++ src/langserv/servers.ts
@@ -1,16 +1,7 @@
-// TODO: TS pls?
-const jayson = require('jayson')
-// TODO: figure out rpc lib that works with net/stdio and has promises/proxies/etc
+import { connect as connectToServerOn, Server } from './channel'
 import { spawn } from 'child_process'
 
-type OnDataCallback = (data: any) => void
-export interface Server {
-  // TODO: call function instead via proxy to rpc
-  write(data: any): void,
-  onData(cb: OnDataCallback): void,
-}
-
-const servers = new Map<string, (port: number | string) => Server>()
+const servers = new Map<string, (port: number) => Server>()
 
 servers.set('javascript', port => {
   spawn('node', [
@@ -18,12 +9,7 @@ servers.set('javascript', port => {
     port + ''
   ])
 
-  let dataCb: OnDataCallback
-
-  return {
-    write: m => console.log(m),
-    onData: cb => dataCb = cb,
-  } as Server
+  return connectToServerOn(port)
 })
 
 servers.set('typescript', port => {
@@ -33,15 +19,13 @@ servers.set('typescript', port => {
     port + ''
   ])
 
-  let dataCb: OnDataCallback
-
-  return {
-    write: m => console.log(m),
-    onData: cb => dataCb = cb,
-  } as Server
+  return connectToServerOn(port)
 })
 
-export const startServerFor = (type: string, port: number | string) => {
+
+export const hasServerFor = (type: string) => servers.has(type)
+
+export const startServerFor = (type: string, port: number) => {
   const starter = servers.get(type)
   if (!starter) return
   return starter(port)
