8a64cea538a3bc7922cdfd3532d843747f6a6f66
Deomitrus <github@deomitrus.com>
2017-09-10 15:41:29 -0400

    handle json rpc batch

--- src/langserv/channel.ts
+++ src/langserv/channel.ts
@@ -1,4 +1,4 @@
-import { ID, Watchers } from '../utils'
+import { is, ID, Watchers } from '../utils'
 import { createConnection } from 'net'
 
 type OnErrorCallback = (error: any) => void
@@ -19,21 +19,24 @@ const uuid = ID()
 const watchers = new Watchers()
 const pendingRequests = new Map<number, Completion>()
 
-// TODO: handle batch (if array of multiple requests/notifications/etc)
-const decode = (buf: Buffer) => {
-  const json = buf.toString()
-  try { return JSON.parse(json) } catch(e) { return {} }
+const encode = (data: any) => JSON.stringify({ ...data, jsonrpc: '2.0' })
+const decode = (buf: Buffer): any[] => {
+  try {
+    let data = JSON.parse(buf.toString())
+    if (is.array(data)) return data
+    if (is.object(data)) return [data]
+    else return []
+  } catch(e) {
+    return []
+  }
 }
-
-const encodeJsonRpc = (data: any) => JSON.stringify({ ...data, jsonrpc: '2.0' })
 
 export const connect = (port: number): Server => {
   let errCb: OnErrorCallback = () => {}
   const client = createConnection(port)
 
-  client.on('data', (buf: Buffer) => {
-    // TODO: handle batch (see decode)
-    const { id, result, error, method, params } = decode(buf)
+  client.on('data', (buf: Buffer) => decode(buf).forEach(data => {
+    const { id, result, error, method, params } = data
 
     if (result && pendingRequests.has(id)) {
       const { done, fail } = pendingRequests.get(id)!
@@ -46,23 +49,23 @@ export const connect = (port: number): Server => {
     watchers.notifyFn(method, fn => {
       const maybePromise = fn(...params)
       if (id && maybePromise && maybePromise.then) maybePromise
-        .then((result: any) => client.write(encodeJsonRpc({ id, result })))
+        .then((result: any) => client.write(encode({ id, result })))
         .catch((e: any) => {
           errCb(e)
-          client.write(encodeJsonRpc({ id, error: { code: -32601, message: 'Method not found' } }))
+          client.write(encode({ id, error: { code: -32601, message: 'Method not found' } }))
         })
     })
-  })
+  }))
 
   const api = {} as Server
 
   api.request = (method, ...params: any[]) => {
     const id = uuid.next()
-    client.write(encodeJsonRpc({ method, params, id }))
+    client.write(encode({ method, params, id }))
     return new Promise((done, fail) => pendingRequests.set(id, { done, fail }))
   }
 
-  api.notify = (method, ...params: any[]) => client.write(encodeJsonRpc({ method, params }))
+  api.notify = (method, ...params: any[]) => client.write(encode({ method, params }))
   api.on = (method, cb) => watchers.add(method, cb)
   api.onError = cb => errCb = cb
 
