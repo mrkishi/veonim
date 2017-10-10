e6252002a6e72a1a6831075b5e9ad2b049b84cfe
Deomitrus <github@deomitrus.com>
2017-10-09 20:29:38 -0400

    move autocomplete logic to ai

--- src/ai.ts
+++ src/ai.ts
@@ -1,18 +1,65 @@
 import { fullBufferUpdate, partialBufferUpdate, references, definition, rename, signatureHelp, hover, symbols, workspaceSymbols } from './langserv/adapter'
-import { ex, action, autocmd, until, cwdir, call, expr, getCurrentLine, feedkeys } from './ui/neovim'
+import { g, ex, action, autocmd, until, cwdir, call, expr, getCurrentLine, feedkeys } from './ui/neovim'
+import { cc, debounce, merge, hasUpperCase, findIndexRight } from './utils'
 import * as harvester from './ui/plugins/keyword-harvester'
+import * as completionUI from './ui/plugins/autocomplete'
 import * as symbolsUI from './ui/plugins/symbols'
 import * as hoverUI from './ui/plugins/hover'
-import { cc, debounce, merge } from './utils'
+import { filter } from 'fuzzaldrin-plus'
 import vimUI from './ui/canvasgrid'
+import { sub } from './dispatch'
 
-let pauseUpdate = false
 interface Cache { startIndex: number, completionItems: string[], filetype: string, file: string, revision: number, cwd: string }
 export const cache: Cache = { filetype: '', file: '', revision: -1, cwd: '', startIndex: 0, completionItems: [] }
+const maxResults = 8
+let pauseUpdate = false
+
+// TODO: get from lang server
+const completionTriggers = new Map<string, RegExp>()
+// TODO: $$$$ sign, reallY?
+completionTriggers.set('javascript', /[^\w\$\-]/)
+completionTriggers.set('typescript', /[^\w\$\-]/)
 
 const fileInfo = () => {
   const { cwd, file, filetype, revision } = cache
   return { cwd, file, filetype, revision }
+}
+
+const orderCompletions = (m: string[], query: string) =>
+  m.slice().sort(a => hasUpperCase(a) ? -1 : a.startsWith(query) ? -1 : 1)
+
+const calcMenuPosition = (startIndex: number, column: number, count: number) => {
+  // anchor menu above row if the maximum results are going to spill out of bounds.
+  // why maxResults instead of the # of items in options? because having the menu jump
+  // around over-under as you narrow down results by typing or undo is kinda annoying
+  const row = vimUI.cursor.row + maxResults > vimUI.rows
+    ? vimUI.cursor.row - count
+    : vimUI.cursor.row + 1
+
+  const start = Math.max(0, startIndex)
+  const col = vimUI.cursor.col - (column - start)
+  return { y: vimUI.rowToY(row), x: vimUI.colToX(col) }
+}
+
+const findQuery = (filetype: string, line: string, column: number) => {
+  const pattern = completionTriggers.get(filetype) || /[^\w\-]/
+  const start = findIndexRight(line, pattern, column - 2) || 0
+  const startIndex = start ? start + 1 : 0
+  const query = line.slice(startIndex, column - 1) || ''
+  const leftChar = line[start]
+  // TODO: should startIndex be modified for leftChar?
+  return { startIndex, query, leftChar }
+}
+
+const getPos = async () => {
+  // TODO: use nvim_window_* api instead or ui.cursor position?
+  const [ buffer, line, column, offset ] = await call.getpos('.')
+  return { buffer, line, column, offset }
+}
+
+const updateVim = (items: string[]) => {
+  cache.completionItems = items
+  g.veonim_completions = items
 }
 
 const updateServer = async (lineChange = false) => {
@@ -42,6 +89,50 @@ const attemptUpdate = async (lineChange = false) => {
   cache.revision = chg
 }
 
+const getCompletions = async () => {
+  // TODO: use neovim api built-ins? better perf? line is slowest. ui.cursor not work as it's global
+  const [ lineData, { column } ] = await cc(getCurrentLine(), getPos())
+  const { startIndex, query } = findQuery(cache.filetype, lineData, column)
+
+  // TODO: if (left char is . or part of the completionTriggers defined per filetype) 
+  if (query.length) {
+    const words = await harvester.getKeywords(cache.cwd, cache.file)
+    if (!words || !words.length) return
+    // TODO: call keywords + semantic = combine -> filter against query
+    // TODO: call once per startIndex. don't repeat call if startIndex didn't change?
+    // TODO: only call this if query has changed 
+
+    // query.toUpperCase() allows the filter engine to rank camel case functions higher
+    // aka: saveUserAccount > suave for query: 'sua'
+    const completions = filter(words, query.toUpperCase(), { maxResults })
+
+    if (!completions.length) {
+      updateVim([])
+      completionUI.hide()
+      return
+    }
+
+    const orderedCompletions = orderCompletions(completions, query)
+    updateVim(orderedCompletions)
+    const options = orderedCompletions.map((text, id) => ({ id, text }))
+    const { x, y } = calcMenuPosition(startIndex, column, options.length)
+    completionUI.show({ options, x, y })
+
+    // TODO: do we always need to update this?
+    // TODO: cache last position in insert session
+    // only update vim if (changed) 
+    // use cache - if (same) dont re-ask for keyword/semantic completions from avo
+    //if (cache.startIndex !== startIndex) {
+    //setVar('veonim_complete_pos', startIndex)
+    //ui.show()
+    //}
+    g.veonim_complete_pos = startIndex
+  } else {
+    completionUI.hide()
+    updateVim([])
+  }
+}
+
 autocmd.bufEnter(debounce(async () => {
   const [ cwd, file, filetype ] = await cc(cwdir(), call.expand(`%f`), expr(`&filetype`))
   // TODO: changedtick -> revision
@@ -51,7 +142,27 @@ autocmd.bufEnter(debounce(async () => {
 
 autocmd.textChanged(debounce(() => attemptUpdate(), 200))
 autocmd.textChangedI(() => attemptUpdate(true))
-autocmd.insertLeave(() => !pauseUpdate && updateServer())
+autocmd.cursorMoved(() => hoverUI.hide())
+autocmd.cursorMovedI(() => {
+  hoverUI.hide()
+  getCompletions()
+})
+
+autocmd.insertLeave(() => {
+  cache.startIndex = 0
+  completionUI.hide()
+  !pauseUpdate && updateServer()
+})
+
+autocmd.completeDone(async () => {
+  g.veonim_completing = 0
+  const { word } = await expr(`v:completed_item`)
+  harvester.addWord(cache.cwd, cache.file, word)
+  updateVim([])
+})
+
+sub('pmenu.select', ix => completionUI.select(ix))
+sub('pmenu.hide', () => completionUI.hide())
 
 action('references', async () => {
   const [ , line, column ] = await call.getpos('.')
@@ -96,9 +207,6 @@ action('hover', async () => {
   const x = vimUI.colToX(column)
   hoverUI.show({ html, x, y })
 })
-
-autocmd.cursorMoved(() => hoverUI.hide())
-autocmd.cursorMovedI(() => hoverUI.hide())
 
 // TODO: this will be auto-triggered. get triggerChars from server.canDo
 // TODO: try to figure out if we are inside func call? too much work? (so this func is not called when outside func)
--- src/ui/plugins/autocomplete.ts
+++ src/ui/plugins/autocomplete.ts
@@ -1,26 +1,10 @@
-import { call, autocmd, g, expr, getCurrentLine } from '../neovim'
-import { cc, findIndexRight, hasUpperCase } from '../../utils'
-import * as harvester from './keyword-harvester'
 import { h, app, Actions } from '../uikit'
-import { filter } from 'fuzzaldrin-plus'
-import { sub } from '../../dispatch'
 import { translate } from '../css'
-import vimUI from '../canvasgrid'
-import { cache } from '../../ai'
-
-const orderCompletions = (m: string[], query: string) =>
-  m.slice().sort(a => hasUpperCase(a) ? -1 : a.startsWith(query) ? -1 : 1)
-
-// TODO: get from lang server
-const completionTriggers = new Map<string, RegExp>()
-// TODO: $$$$ sign, reallY?
-completionTriggers.set('javascript', /[^\w\$\-]/)
-completionTriggers.set('typescript', /[^\w\$\-]/)
 
 interface CompletionOption { id: number, text: string }
 interface State { options: CompletionOption[], vis: boolean, ix: number, x: number, y: number }
+interface ShowParams { x: number, y: number, options: CompletionOption[] }
 
-const maxResults = 8
 const state: State = { options: [], vis: false, ix: 0, x: 0, y: 0 }
 
 const view = ({ options, vis, ix, x, y }: State) => h('#autocomplete.plugin', {
@@ -48,97 +32,6 @@ a.select = (_s, _a, ix: number) => ({ ix })
 
 const ui = app({ state, view, actions: a }, false)
 
-const findQuery = (filetype: string, line: string, column: number) => {
-  const pattern = completionTriggers.get(filetype) || /[^\w\-]/
-  const start = findIndexRight(line, pattern, column - 2) || 0
-  const startIndex = start ? start + 1 : 0
-  const query = line.slice(startIndex, column - 1) || ''
-  const leftChar = line[start]
-  // TODO: should startIndex be modified for leftChar?
-  return { startIndex, query, leftChar }
-}
-
-const getPos = async () => {
-  // TODO: use nvim_window_* api instead or ui.cursor position?
-  const [ buffer, line, column, offset ] = await call.getpos('.')
-  return { buffer, line, column, offset }
-}
-
-const updateVim = (items: string[]) => {
-  cache.completionItems = items
-  g.veonim_completions = items
-}
-
-const calcMenuPosition = (startIndex: number, column: number, count: number) => {
-  // anchor menu above row if the maximum results are going to spill out of bounds.
-  // why maxResults instead of the # of items in options? because having the menu jump
-  // around over-under as you narrow down results by typing or undo is kinda annoying
-  const row = vimUI.cursor.row + maxResults > vimUI.rows
-    ? vimUI.cursor.row - count
-    : vimUI.cursor.row + 1
-
-  const start = Math.max(0, startIndex)
-  const col = vimUI.cursor.col - (column - start)
-  return { y: vimUI.rowToY(row), x: vimUI.colToX(col) }
-}
-
-const getCompletions = async () => {
-  // TODO: use neovim api built-ins? better perf? line is slowest. ui.cursor not work as it's global
-  const [ lineData, { column } ] = await cc(getCurrentLine(), getPos())
-  const { startIndex, query } = findQuery(cache.filetype, lineData, column)
-
-  // TODO: if (left char is . or part of the completionTriggers defined per filetype) 
-  if (query.length) {
-    const words = await harvester.getKeywords(cache.cwd, cache.file)
-    if (!words || !words.length) return
-    // TODO: call keywords + semantic = combine -> filter against query
-    // TODO: call once per startIndex. don't repeat call if startIndex didn't change?
-    // TODO: only call this if query has changed 
-
-    // query.toUpperCase() allows the filter engine to rank camel case functions higher
-    // aka: saveUserAccount > suave for query: 'sua'
-    const completions = filter(words, query.toUpperCase(), { maxResults })
-
-    if (!completions.length) {
-      updateVim([])
-      ui.hide()
-      return
-    }
-
-    const orderedCompletions = orderCompletions(completions, query)
-    updateVim(orderedCompletions)
-    const options = orderedCompletions.map((text, id) => ({ id, text }))
-    const { x, y } = calcMenuPosition(startIndex, column, options.length)
-    ui.show({ options, x, y })
-
-    // TODO: do we always need to update this?
-    // TODO: cache last position in insert session
-    // only update vim if (changed) 
-    // use cache - if (same) dont re-ask for keyword/semantic completions from avo
-    //if (cache.startIndex !== startIndex) {
-      //setVar('veonim_complete_pos', startIndex)
-      //ui.show()
-    //}
-    g.veonim_complete_pos = startIndex
-  } else {
-    ui.hide()
-    updateVim([])
-  }
-}
-
-sub('pmenu.select', ix => ui.select(ix))
-sub('pmenu.hide', () => ui.hide())
-
-autocmd.cursorMovedI(() => getCompletions())
-
-autocmd.completeDone(async () => {
-  g.veonim_completing = 0
-  const { word } = await expr(`v:completed_item`)
-  harvester.addWord(cache.cwd, cache.file, word)
-  updateVim([])
-})
-
-autocmd.insertLeave(() => {
-  cache.startIndex = 0
-  ui.hide()
-})
+export const show = (params: ShowParams) => ui.show(params)
+export const select = (index: number) => ui.select(index)
+export const hide = () => ui.hide()
