cc163a6bf45ccbc3abad3831451aba00a6b32aba
goavn <goavn@deomitrus.com>
2017-10-16 16:57:07 -0400

    Delete recursedir.js

--- recursedir.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const fs = require('fs')
-
-const read = dir => fs.readdirSync(dir).reduce((files, file) => fs.statSync(path.join(dir, file)).isDirectory() 
-  ? files.concat(read(path.join(dir, file))) 
-  : files.concat(path.join(dir, file)), [])
-
-exports.default = module.exports = read
