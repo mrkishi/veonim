c4aad535cde7b4ebf1e1f265b64ab3c264589f99
Deomitrus <github@deomitrus.com>
2017-12-24 23:56:19 -0500

    invert cursor color

--- src/core/cursor.ts
+++ src/core/cursor.ts
@@ -10,31 +10,36 @@ export enum CursorShape {
   underline,
 }
 
-export const cursor = { row: 0, col: 0, color: '#fff' }
+export const cursor = { row: 0, col: 0, color: '#fff', type: CursorShape.block }
 const cursorEl = document.getElementById('cursor') as HTMLElement
+const cursorChar = document.createElement('span')
+
 merge(cursorEl.style, {
   position: 'absolute',
-  color: '#000',
-  'mix-blend-mode': 'difference',
+  display: 'flex',
+  'justify-content': 'center',
+  'align-items': 'center',
 })
 
+cursorChar.style.filter = 'invert(1) grayscale(1)'
+cursorEl.appendChild(cursorChar)
+
 export const setCursorShape = (type: CursorShape, size = 20) => {
+  cursor.type = type
+
   if (type === CursorShape.block) merge(cursorEl.style, {
-    //'mix-blend-mode': 'difference',
     background: cursor.color,
     height: `${canvasContainer.cell.height}px`,
     width: `${canvasContainer.cell.width}px`,
   })
 
   if (type === CursorShape.line) merge(cursorEl.style, {
-    //'mix-blend-mode': 'normal',
     background: cursor.color,
     height: `${canvasContainer.cell.height}px`,
     width: `${(canvasContainer.cell.width * (size / 100)).toFixed(2)}px`
   })
 
   if (type === CursorShape.underline) merge(cursorEl.style, {
-    //'mix-blend-mode': 'normal',
     background: partialFill('horizontal', cursor.color, size),
     height: `${canvasContainer.cell.height}px`,
     width: `${canvasContainer.cell.width}px`,
@@ -42,21 +47,30 @@ export const setCursorShape = (type: CursorShape, size = 20) => {
 }
 
 export const setCursorColor = (color: string) => {
-  // TODO: invert text color. now that we have grid cache this should be more feasible
+  cursorChar.style.color = color
   cursor.color = color
-  // TODO: disable fancy blending option
   cursorEl.style.background = color
 }
 
 export const hideCursor = () => merge(cursorEl.style, { display: 'none' })
 export const showCursor = () => merge(cursorEl.style, { display: 'block' })
+
 export const moveCursor = () => {
   const win = getWindow(cursor.row, cursor.col)
   if (!win) return
+
   cursorEl.style.transform = translate(win.colToX(cursor.col), win.rowToY(cursor.row))
-  const [ char ] = get(cursor.row, cursor.col)
-  //cursorEl.innerText = char
-  console.log('char at cusror:', char)
+
+  if (cursor.type === CursorShape.block) {
+    const [ char ] = get(cursor.row, cursor.col)
+    cursorChar.innerText = char
+    cursorChar.style.display = ''
+  }
+
+  else {
+    cursorChar.style.display = 'none'
+    cursorChar.innerText = ''
+  }
 }
 
 setCursorShape(CursorShape.block)
