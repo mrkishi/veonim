818f4fbf246af341f01933224283bb034bd374f6
Deomitrus <github@deomitrus.com>
2018-01-07 02:07:08 -0500

    restyle vim switch session

--- src/components/vim-switch.ts
+++ src/components/vim-switch.ts
@@ -1,26 +1,43 @@
+import { h, app, Actions, ActionCaller } from '../ui/uikit'
 import { list, switchVim } from '../core/sessions'
-import { h, app, Actions } from '../ui/uikit'
-import TermInput from '../components/input'
+import { Plugin, Row } from '../styles/common'
+import Input from '../components/text-input'
 import { filter } from 'fuzzaldrin-plus'
 import { action } from '../core/neovim'
 
-interface Session { id: number, name: string }
-interface State { val: string, vis: boolean, list: Session[], cache: Session[], ix: number }
-const state: State = { val: '', vis: false, list: [], cache: [], ix: 0 }
+interface Session {
+  id: number,
+  name: string,
+}
 
-const view = ({ val, vis, list, ix }: State, { select, hide, change, next, prev }: any) => h('#vim-switch.plugin', {
-  hide: !vis
-}, [
-  h('.dialog.small', [
-    TermInput({ focus: true, val, select, hide, change, next, prev }),
+interface State {
+  val: string,
+  vis: boolean,
+  list: Session[],
+  cache: Session[],
+  ix: number,
+}
 
-    h('.row', { render: !list.length }, '...'),
+const state: State = {
+  val: '',
+  vis: false,
+  list: [],
+  cache: [],
+  ix: 0,
+}
 
-    h('div', list.map((s, key: number) => h('.row', {
-      key,
-      css: { active: key === ix },
-    }, s.name))),
-  ])
+const view = ($: State, actions: ActionCaller) => Plugin.default('vim-switch', $.vis, [
+
+  ,Input({
+    ...actions,
+    val: $.val,
+    focus: true,
+    icon: 'search',
+    desc: 'switch vim session',
+  })
+
+  ,h('div', $.list.map((s, key) => Row.normal({ key, activeWhen: key === $.ix }, s.name)))
+
 ])
 
 const a: Actions<State> = {}
@@ -39,6 +56,7 @@ a.select = (s, a) => {
   a.hide()
 }
 
+// TODO: don't limit list to 10 entries and scroll instead!
 a.next = s => ({ ix: s.ix + 1 > Math.min(s.list.length - 1, 9) ? 0 : s.ix + 1 })
 a.prev = s => ({ ix: s.ix - 1 < 0 ? Math.min(s.list.length - 1, 9) : s.ix - 1 })
 
