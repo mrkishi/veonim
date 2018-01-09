e853ef0b95e219c1afce511603c3b00d41b9e190
Deomitrus <github@deomitrus.com>
2018-01-08 19:40:00 -0500

    add hide action and remove junk

--- src/components/xform.ts
+++ src/components/xform.ts
@@ -1,22 +1,10 @@
-import { action, getCurrent, Highlight } from '../core/neovim'
+import { action } from '../core/neovim'
 import { remote } from 'electron'
 
+action('hide', () => remote.app.hide())
 action('quit', () => remote.app.quit())
 action('devtools', () => remote.getCurrentWebContents().toggleDevTools())
 action('fullscreen', () => {
   const win = remote.getCurrentWindow()
   win.setFullScreen(!win.isFullScreen())
-})
-
-let currentHighlight = 0
-
-action('uadd', async () => {
-  const buffer = await getCurrent.buffer
-  currentHighlight = await buffer.addHighlight(-1, Highlight.Undercurl, 9, 18, 22)
-  console.log('hi id:', currentHighlight)
-})
-
-action('uclear', async () => {
-  const buffer = await getCurrent.buffer
-  buffer.clearAllHighlights(currentHighlight)
 })
