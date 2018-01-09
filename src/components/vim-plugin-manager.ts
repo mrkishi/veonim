c5f01e553b266963ad0c56d00e450e8b6b26f999
Deomitrus <github@deomitrus.com>
2018-01-08 20:49:13 -0500

    plugin-manager uses notifications instead of its own ui

--- src/components/vim-plugin-manager.ts
+++ src/components/vim-plugin-manager.ts
@@ -1,46 +1,22 @@
 import { read as readPluginsFromVimrc, install, remove, removeExtraneous, Plugin } from '@veonim/plugin-manager'
 import { watchConfig } from '../config/config-reader'
-import { h, app, Actions } from '../ui/uikit'
+import * as dispatch from '../messaging/dispatch'
 import { action, cmd } from '../core/neovim'
-import { delay } from '../support/utils'
 
-interface State { ready: number, total: number, vis: boolean, loading: boolean }
-const state = { ready: 0, total: 0, vis: false, loading: false }
-
-const view = ({ ready, total, loading, vis }: State, { hide }: any) => h('#vim-plugins.plugin.top', {
-  hide: !vis
-}, [
-  h('.alert', [
-    h('.message', { render: loading }, `Installing ${ready}/${total} Vim plugins...`),
-    h('.message', { render: !loading }, `Installed ${total} Vim plugins`),
-    h('div', { style: { display: 'flex' } }, [
-      // TODO: NO MAUSSS!
-      h('button', {
-        onclick: hide,
-        style: { 'flex': 1, }
-      }, 'ok, whatever'),
-    ]),
-  ])
-])
-
-const a: Actions<State> = {}
-a.show = (_s, _a, total) => ({ total, vis: true, loading: true })
-a.hide = () => ({ ready: 0, total: 0, vis: false, loading: false })
-a.installTick = s => ({ ready: s.ready + 1 })
-a.done = () => ({ loading: false })
-
-const ui = app({ state, view, actions: a }, false)
+// TODO: support other plugin host sites besides github.com?
+// TODO: move out of components (no UI) -> services folder?
 
 const installPlugins = async (plugins: Plugin[], { reinstall = false } = {}) => {
   if (!plugins.length) return removeExtraneous()
-  ui.show(plugins.length)
+  dispatch.pub('notification:system', `Found ${plugins.length} Vim plugins. Installing...`)
+
   if (reinstall) await remove(plugins)
-  await Promise.all(plugins.map(p => install(p).then(() => ui.installTick())))
-  ui.done()
+  // TODO: show install progress somehow
+  await Promise.all(plugins.map(p => install(p)))
+  dispatch.pub('notification:success', `Installed ${plugins.length} Vim plugins!`)
+
   removeExtraneous()
   cmd(`packloadall!`)
-  await delay(3e3)
-  ui.hide()
 }
 
 const refreshPlugins = () => readPluginsFromVimrc().then(async plugins => installPlugins(plugins.filter(p => !p.installed)))
@@ -48,5 +24,3 @@ action('reinstall-plugins', () => readPluginsFromVimrc().then(plugins => install
 
 refreshPlugins()
 watchConfig('nvim/init.vim', () => refreshPlugins())
-
-// TODO: support other plugin host sites besides github.com?
