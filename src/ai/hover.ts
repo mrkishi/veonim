c02e2e35216b401ff2ed46bcc3c55170d02c7383
Deomitrus <github@deomitrus.com>
2018-01-10 20:52:55 -0500

    use modern ts version of remove-markdown

--- src/ai/hover.ts
+++ src/ai/hover.ts
@@ -1,5 +1,5 @@
 import { action, current as vimState, on } from '../core/neovim'
-const removeMarkdown = require('remove-markdown')
+import * as markdown from '../support/markdown'
 import * as hoverUI from '../components/hover'
 import { hover } from '../langserv/adapter'
 import Worker from '../messaging/worker'
@@ -15,7 +15,7 @@ export const colorizer = Worker('neovim-colorizer')
 action('hover', async () => {
   const text = await hover(vimState)
   if (!text) return
-  const cleanData = removeMarkdown(text)
+  const cleanData = markdown.remove(text)
   const data: ColorData[][] = await colorizer.request.colorize(cleanData, vimState.filetype)
 
   hoverUI.show({ data, row: cursor.row, col: cursor.col })
