e6a81c54d9b9ebd2bf3efa9e5c61a6c0bf8e6a2f
Deomitrus <github@deomitrus.com>
2018-01-21 23:45:12 -0500

    buffer job and parse job output

--- src/services/job-reader.ts
+++ src/services/job-reader.ts
@@ -1,6 +1,19 @@
 import { systemAction } from '../core/neovim'
 
+const bufferings = new Map<number, string[]>()
+const buffer = (id: number, stuff: string[]) => bufferings.has(id)
+  ? bufferings.get(id)!.push(...stuff)
+  : bufferings.set(id, stuff)
+
 systemAction('job-output', (jobId: number, data: string[]) => {
-  // TODO: if last line === "" then buffer complete. otherwise more to come!
-  console.log(jobId, data.join('\n'))
+  const lastLine = data[data.length - 1]
+
+  if (lastLine === '') {
+    const prevMsg = bufferings.get(jobId) || []
+    bufferings.delete(jobId)
+    const msg = [...prevMsg, ...data]
+    console.log(msg.join('\n'))
+  }
+
+  else buffer(jobId, data)
 })
