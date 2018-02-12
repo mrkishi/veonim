1289ed7498af89a33c4852d5583b032ae8a8a21f
breja <gh@breja.io>
2018-02-12 18:57:35 -0500

    as todo

--- src/ai/completion-transforms.ts
+++ src/ai/completion-transforms.ts
@@ -1,7 +1,7 @@
 import { CompletionOption, CompletionKind } from '../ai/completions'
 import { is } from '../support/utils'
 import { parse } from 'path'
-// uhh.. this doesn't belong here
+// TODO: uhh.. this doesn't belong here
 // import '../support/find-node-modules'
 
 export interface CompletionTransformRequest {
