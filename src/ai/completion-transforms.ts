5d27a1c0f98ff769669fe9c2bfa68a42e1146abb
breja <gh@breja.io>
2018-02-12 13:45:45 -0500

    fill out baseline for typescript path transforms

--- src/ai/completion-transforms.ts
+++ src/ai/completion-transforms.ts
@@ -1,5 +1,6 @@
 import { CompletionOption, CompletionKind } from '../ai/completions'
 import { is } from '../support/utils'
+import { parse } from 'path'
 
 export interface CompletionTransformRequest {
   completionKind: CompletionKind,
@@ -23,12 +24,21 @@ export default (filetype: string, request: CompletionTransformRequest) => {
 }
 
 const isModuleImport = (lineContent: string, column: number) => {
-
+  const fragment = lineContent.slice(0, column)
+  return /\b(from|import)\s*["'][^'"]*$/.test(fragment)
+    || /\b(import|require)\(['"][^'"]*$/.test(fragment)
 }
 
 transforms.set('typescript', m => {
   // in the future these can be separated into different modules and organized
   // better. for MVP this will be good enough
-  if (m.completionKind !== CompletionKind.Path) return
+  if (m.completionKind !== CompletionKind.Path) return m.completionOptions
 
+  const tryingToCompleteInsideImportPath = isModuleImport(m.lineContent, m.column)
+  if (!tryingToCompleteInsideImportPath) return m.completionOptions
+
+  return m.completionOptions.map(o => ({
+    ...o,
+    insertText: parse(o.text).name,
+  }))
 })
