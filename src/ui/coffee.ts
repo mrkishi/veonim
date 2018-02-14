60c3d1534d78287b6fa99b68707158ea5e226268
breja <gh@breja.io>
2018-02-13 21:19:23 -0500

    coffee ready

--- src/ui/coffee.ts
+++ src/ui/coffee.ts
@@ -1,20 +1,3 @@
-// not like i need the typings for react, but... if i install @types/react the
-// webworkers tsconfig goes apeshit trying to include it for some unknown
-// reason - WITHOUT ACTUALLY WRITING ANY CODE THAT HAS THE WORD 'react' IN IT.
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// WHAT THE FUCK
-// web development is a meme.
-
 let reactModule = 'react/umd/react.production.min'
 let reactDomModule = 'react-dom/umd/react-dom.production.min.js'
 
@@ -27,13 +10,4 @@ const ReactDom = require(reactDomModule)
 import huu from 'huu'
 
 export const h = huu(React.createElement)
-
-const hostElement = document.getElementById('plugins') as HTMLElement
-const targetEl = document.createElement('div')
-hostElement.appendChild(targetEl)
-
-const view = h('div', [
-  ,h('span', 'hey')
-])
-
-ReactDom.render(view, targetEl)
+export const renderDom = (vNode: any, element: HTMLElement) => ReactDom.render(vNode, element)
