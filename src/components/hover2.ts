476c203549e6f895e4221b0915558a9a23f8fc0a
breja <gh@breja.io>
2018-02-14 21:06:10 -0500

    begin building hover in react

---
+++ src/components/hover2.ts
@@ -0,0 +1,42 @@
+import { font } from '../core/canvas-container'
+import { Hover } from '../state/s-hover'
+import { paddingVH } from '../ui/css'
+import { h } from '../ui/coffee'
+
+const docs = (data: string) => h('div', {
+  style: {
+    ...paddingVH(8, 6),
+    overflow: 'visible',
+    whiteSpace: 'normal',
+    background: 'var(--background-45)',
+    color: 'var(--foreground-40)',
+    fontSize: `${font.size - 2}px`,
+  }
+}, data)
+
+const view = ($: Hover) => h('div', [
+
+  ,$.doc && !$.anchorBottom && docs($.doc)
+
+  ,h('div', {
+    style: {
+      background: 'var(--background-30)',
+      padding: '8px',
+    }
+  }, $.value.map(m => h('div', {
+    style: {
+      display: 'flex',
+      flexFlow: 'row wrap',
+    }
+  }, m.map(({ color, text }) => h('span', {
+    style: {
+      color: color || 'var(--foreground)',
+      whiteSpace: 'pre',
+    }
+  }, text)))))
+
+  ,$.doc && $.anchorBottom && docs($.doc)
+
+])
+
+export default view
