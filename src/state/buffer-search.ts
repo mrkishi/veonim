21c66daa2e52c1a5719b5a76297b7df0fe97154a
breja <gh@breja.io>
2018-02-20 16:43:04 -0500

    buffer search find result locations

--- src/state/buffer-search.ts
+++ src/state/buffer-search.ts
@@ -2,6 +2,18 @@ import { on, initState, go } from '../state/trade-federation'
 import { current as vim } from '../core/neovim'
 import { finder } from '../ai/update-server'
 
+interface FilterResult {
+  line: string,
+  start: {
+    line: number,
+    column: number,
+  },
+  end: {
+    line: number,
+    column: number,
+  }
+}
+
 export interface BufferSearch {
   value: string,
   options: string[],
@@ -23,7 +35,10 @@ export interface Actions {
 
 on.updateBufferSearchQuery((s, query) => {
   s.bufferSearch.value = query
-  finder.request.query(vim.cwd, vim.file, query).then(go.updateBufferSearchOptions)
+  finder.request.query(vim.cwd, vim.file, query).then((results: FilterResult[]) => {
+    console.log('filter results:', results)
+    go.updateBufferSearchOptions(results.map(m => m.line))
+  })
 })
 
 on.updateBufferSearchOptions((s, options) => s.bufferSearch.options = options)
--- src/workers/buffer-search.ts
+++ src/workers/buffer-search.ts
@@ -1,15 +1,46 @@
 import WorkerClient from '../messaging/worker-client'
-import { filter as fuzzy } from 'fuzzaldrin-plus'
+import { filter as fuzzy, match } from 'fuzzaldrin-plus'
 import { join } from 'path'
 
-const { on } = WorkerClient()
+interface FilterResult {
+  line: string,
+  start: {
+    line: number,
+    column: number,
+  },
+  end: {
+    line: number,
+    column: number,
+  }
+}
 
+const { on } = WorkerClient()
 const buffers = new Map<string, string[]>()
 
-const filter = (cwd: string, file: string, query: string, maxResults = 20): string[] => {
-  const results = fuzzy(buffers.get(join(cwd, file)) || [], query, { maxResults })
-  return [...new Set(results)]
+const getLocations = (str: string, query: string, buffer: string[]) => {
+  const line = buffer.indexOf(str)
+  const locations = match(str, query)
+
+  return {
+    start: { line, column: locations[0] },
+    end: { line, column: locations[locations.length - 1] },
+  }
 }
+
+const filter = (cwd: string, file: string, query: string, maxResults = 20): FilterResult[] => {
+  const bufferData = buffers.get(join(cwd, file)) || []
+  const results = fuzzy(bufferData, query, { maxResults })
+
+  return [...new Set(results)]
+    .map(m => ({
+      line: m,
+      ...getLocations(m, query, bufferData),
+    }))
+}
+
+// TODO: allow query over a range of lines this would be useful to first search
+// over only the lines visible in the current vim window. if not found, only
+// then search entire buffer.
 
 on.set((cwd: string, file: string, buffer: string[]) => buffers.set(join(cwd, file), buffer))
 
