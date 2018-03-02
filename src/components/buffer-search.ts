0b00f26b6e510909a417c7de3aa277ef09ed6272
breja <gh@breja.io>
2018-03-02 14:19:46 -0500

    tweak buffer search position and todo

--- src/components/buffer-search.ts
+++ src/components/buffer-search.ts
@@ -1,17 +1,21 @@
-import { Plugin } from '../components/plugin-container'
+import { PluginBottom } from '../components/plugin-container'
 import { connect, go } from '../state/trade-federation'
 import { BufferSearch } from '../state/buffer-search'
 import Input from '../components/text-input2'
 import { normal } from '../core/neovim'
 
-const view = ({ data: $ }: { data: BufferSearch }) => Plugin('buffer-search', $.visible, [
+const view = ({ data: $ }: { data: BufferSearch }) => PluginBottom('buffer-search', $.visible, [
 
   ,Input({
+    small: true,
     focus: true,
     value: $.value,
     icon: 'search',
     hide: go.hideBufferSearch,
     change: val => go.updateBufferSearchQuery(val),
+    // TODO: this jump is not good enough.
+    // should get line + column of top fuzzy match
+    // then verify if current cursor is at line+column, if not move it there
     select: () => go.hideBufferSearch() && normal(`nN`),
   })
 
