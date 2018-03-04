144348a908810422da9ece491ac2114a3f79a9bb
breja <gh@breja.io>
2018-03-04 16:53:13 -0500

    attempt live reload

--- src/bootstrap/main.ts
+++ src/bootstrap/main.ts
@@ -26,6 +26,26 @@ app.on('ready', async () => {
   win.loadURL(`file:///${__dirname}/index.html`)
 
   if (process.env.VEONIM_DEV) {
+    function debounce (fn: Function, wait = 1) {
+      let timeout: NodeJS.Timer
+      return function(this: any, ...args: any[]) {
+        const ctx = this
+        clearTimeout(timeout)
+        timeout = setTimeout(() => fn.apply(ctx, args), wait)
+      }
+    }
+
+    const watch = require('node-watch')
+    const srcDir = require('path').resolve(__dirname, '../../build')
+    console.log('scrdir:', srcDir)
+
+    const reloader = () => {
+      console.log('reloading changes...')
+      win.webContents.reload()
+    }
+
+    watch(srcDir, { recursive: true }, debounce(reloader, 250))
+
     console.log(`veonim started in develop mode. you're welcome`)
 
     const {
--- tools/start.js
+++ tools/start.js
@@ -1,13 +1,65 @@
 'use strict'
 
-const { spawn } = require('child_process')
-const proc = spawn('npx', [
-  'electron',
-  'build/bootstrap/main.js'
-], {
-  shell: true,
-  env: Object.assign({}, process.env, { VEONIM_DEV: 42 })
+const { spawn, exec } = require('child_process')
+const watch = require('node-watch')
+const cwd = `${__dirname}/..`
+const npmrun = task => exec(`npm run ${task}`, { cwd })
+const tsc = conf => spawn('tsc', [
+  '-p',
+  conf,
+  '--watch',
+  '--preserveWatchOutput',
+], { cwd })
+
+let electronStarted = false
+
+const startElectron = () => {
+  electronStarted = true
+
+  const proc = spawn('npx', [
+    'electron',
+    'build/bootstrap/main.js'
+  ], {
+    shell: true,
+    env: Object.assign({}, process.env, { VEONIM_DEV: 42 })
+  })
+
+  proc.stdout.pipe(process.stdout)
+  proc.stderr.pipe(process.stderr)
+}
+
+watch(`${cwd}/src/bootstrap/index.html`, () => {
+  console.log('html modified... copying...')
+  npmrun('html')
 })
 
-proc.stdout.pipe(process.stdout)
-proc.stderr.pipe(process.stderr)
+let initialCompile = 0
+
+tsc('tsconfig.json').stdout.on('data', m => {
+  const line = m.toString()
+  process.stdout.write(line)
+  if (line.includes('Compilation complete')) {
+    initialCompile++
+    if (initialCompile > 1 && !electronStarted) startElectron()
+  }
+})
+
+tsc('src/workers/tsconfig.json').stdout.on('data', m => {
+  const line = m.toString()
+  process.stdout.write(`WW: ${line}`)
+  if (line.includes('Compilation complete')) {
+    initialCompile++
+    if (initialCompile > 1 && !electronStarted) startElectron()
+    console.log('cleaning up exports in web workers...')
+    npmrun('fixexp')
+  }
+})
+
+setTimeout(() => {
+  console.log('copying runtime files')
+  npmrun('runtime')
+  console.log('copying html')
+  npmrun('html')
+  console.log('copying assets')
+  npmrun('assets')
+}, 2e3)
