cc9aeee2fad13a82c88cef800a85b2c5e102ade5
breja <gh@breja.io>
2018-03-06 09:11:42 -0500

    document highlights

--- src/ai/highlights.ts
+++ src/ai/highlights.ts
@@ -1,6 +1,26 @@
-import { highlights, references } from '../langserv/adapter'
-import { action, current as vim } from '../core/neovim'
+import { action, current as vim, getCurrent, cmd, onStateChange } from '../core/neovim'
+import { highlights, references, VimQFItem } from '../langserv/adapter'
 import { canCall } from '../langserv/director'
+import { brighten } from '../ui/css'
+
+// has no meaning, just need a random id representing document highlights
+const HL_ID = 900422
+const HL_GROUP = 'VeonimDocumentHighlight'
+
+const setHighlightColor = () => {
+  const highlightColor = brighten(vim.bg, 25)
+  cmd(`highlight ${HL_GROUP} guibg=${highlightColor}`)
+}
+
+onStateChange.colorscheme(() => setHighlightColor())
+setHighlightColor()
+
+// TODO: should really normalize positions...
+const asHighlight = (m: VimQFItem) => ({
+  line: m.line - 1,
+  start: m.column - 1,
+  end: m.endColumn - 1,
+})
 
 action('highlight', async () => {
   const highlightFeatureEnabled = canCall(vim.cwd, vim.filetype, 'documentHighlight')
@@ -8,11 +28,12 @@ action('highlight', async () => {
     ? await highlights(vim)
     : await references(vim)
 
+  const buffer = await getCurrent.buffer
+  buffer.clearHighlight(HL_ID, 1, -1)
 
-  // TODO: create new highlight group
-  // use nvim api to highlight positions
+  if (!positions.length) return
 
-  if (process.env.VEONIM_DEV) {
-    console.log('highlights', positions)
-  }
+  positions
+    .map(asHighlight)
+    .forEach(hi => buffer.addHighlight(HL_ID, HL_GROUP, hi.line, hi.start, hi.end))
 })
