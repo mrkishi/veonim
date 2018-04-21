c8d22b9afacb102061c1374ab11fb705e8835e3d
breja <gh@breja.io>
2018-04-21 19:23:19 -0400

    change-project to react

--- src/components/change-project.ts
+++ src/components/change-project.ts
@@ -1,12 +1,13 @@
 import { getDirFiles, exists, pathRelativeToHome, simplifyPath, absolutePath } from '../support/utils'
+import { RowNormal, RowImportant } from '../components/row-container'
 import { action, current, cmd, onStateChange } from '../core/neovim'
 import { createVim, renameCurrentToCwd } from '../core/sessions'
-import { h, app, Actions, ActionCaller } from '../ui/uikit'
+import { Plugin } from '../components/plugin-container'
 import configReader from '../config/config-service'
-import { Plugin, Row } from '../styles/common'
 import config from '../config/config-service'
-import Input from '../components/text-input'
+import Input from '../components/text-input2'
 import { filter } from 'fuzzaldrin-plus'
+import { h, app } from '../ui/uikit2'
 import { join, sep } from 'path'
 import { homedir } from 'os'
 
@@ -18,27 +19,18 @@ interface FileDir {
   dir: boolean,
 }
 
-interface State {
-  val: string,
-  cwd: string,
-  path: string,
-  paths: FileDir[],
-  cache: FileDir[],
-  vis: boolean,
-  ix: number,
-  create: boolean,
-}
-
-const state: State = {
-  val: '',
+const state = {
+  value: '',
   cwd: '',
   path: '',
-  paths: [],
-  cache: [],
-  vis: false,
-  ix: 0,
+  paths: [] as FileDir[],
+  cache: [] as FileDir[],
+  visible: false,
+  index: 0,
   create: false,
 }
+
+type S = typeof state
 
 const absPath = (path = '') => path.startsWith('~') ? join($HOME, path.slice(1)) : path
 const validPath = async (path = '') => {
@@ -52,92 +44,112 @@ const filterDirs = (filedirs: FileDir[]) => filedirs.filter(f => f.dir && !ignor
 
 let listElRef: HTMLElement
 
-const view = ($: State, actions: ActionCaller) => Plugin.default('change-dir', $.vis, [
+const resetState = { value: '', path: '', visible: false, index: 0 }
+
+const actions = {
+  select: (s: S) => {
+    if (!s.paths.length) return resetState
+    const { name } = s.paths[s.index]
+    if (!name) return
+    const dirpath = join(s.path, name)
+    s.create ? createVim(name, dirpath) : cmd(`cd ${dirpath}`)
+    return resetState
+  },
+
+  change: (s: S, value: string) => ({ value, paths: value
+    ? filterDirs(filter(s.paths, value, { key: 'name' }))
+    : s.cache
+  }),
+
+  tab: (s: S) => {
+    if (!s.paths.length) return resetState
+    const { name } = s.paths[s.index]
+    if (!name) return
+    const path = join(s.path, name)
+    getDirFiles(path).then(paths => ui.show({ path, paths: filterDirs(paths) }))
+  },
+
+  jumpNext: (s: S) => {
+    const { name, dir } = s.paths[s.index]
+    if (!dir) return
+    const path = join(s.path, name)
+    getDirFiles(path).then(paths => ui.show({ path, paths: filterDirs(paths) }))
+  },
+
+  jumpPrev: (s: S) => {
+    const next = s.path.split(sep)
+    next.pop()
+    const path = join(sep, ...next)
+    getDirFiles(path).then(paths => ui.show({ path, paths: filterDirs(paths) }))
+  },
+
+  show: (s: S, { paths, path, cwd = s.cwd, create }: any) => ({
+    cwd, path, paths, create,
+    index: 0,
+    value: '',
+    visible: true,
+    cache: paths,
+  }),
+
+  // TODO: be more precise than this? also depends on scaled devices
+  down: (s: S) => {
+    listElRef.scrollTop += 300
+    return { index: Math.min(s.index + 17, s.paths.length - 1) }
+  },
+
+  up: (s: S) => {
+    listElRef.scrollTop -= 300
+    return { index: Math.max(s.index - 17, 0) }
+  },
+
+  top: () => { listElRef.scrollTop = 0 },
+  bottom: () => { listElRef.scrollTop = listElRef.scrollHeight },
+  hide: () => resetState,
+  next: (s: S) => ({ index: s.index + 1 >= s.paths.length ? 0 : s.index + 1 }),
+  prev: (s: S) => ({ index: s.index - 1 < 0 ? s.paths.length - 1 : s.index - 1 }),
+}
+
+const ui = app({ name: 'change-project', state, actions, view: ($, a) => Plugin($.visible, [
 
   ,Input({
-    ...actions,
-    val: $.val,
+    up: a.up,
+    top: a.top,
+    tab: a.tab,
+    next: a.next,
+    prev: a.prev,
+    down: a.down,
+    hide: a.hide,
+    select: a.select,
+    change: a.change,
+    bottom: a.bottom,
+    jumpNext: a.jumpNext,
+    jumpPrev: a.jumpPrev,
+    value: $.value,
     focus: true,
     icon: 'home',
     desc: $.create ? 'create new vim session with project' : 'change project',
   })
 
-  ,Row.important(pathRelativeToHome($.path))
+  ,h(RowImportant, [
+    ,h('span', pathRelativeToHome($.path))
+  ])
 
   ,h('div', {
-    onupdate: (e: HTMLElement) => listElRef = e,
+    ref: (e: HTMLElement) => {
+      if (e) listElRef = e
+    },
     style: {
       maxHeight: '50vh',
       overflowY: 'hidden',
     }
-  }, $.paths.map(({ name }, key) => Row.normal({ key, activeWhen: key === $.ix }, name)))
+  }, $.paths.map(({ name }, ix) => h(RowNormal, {
+    key: name,
+    active: ix === $.index,
+  }, [
+    ,h('span', name)
+  ])))
 
-])
-
-const a: Actions<State> = {}
-
-a.select = (s, a) => {
-  if (!s.paths.length) return a.hide()
-  const { name } = s.paths[s.ix]
-  if (!name) return
-  const dirpath = join(s.path, name)
-  s.create ? createVim(name, dirpath) : cmd(`cd ${dirpath}`)
-  return a.hide()
-}
-
-a.change = (s, _a, val: string) => ({ val, paths: val
-  ? filterDirs(filter(s.paths, val, { key: 'name' }))
-  : s.cache
-})
-
-a.tab = (s, a) => {
-  if (!s.paths.length) return a.hide()
-  const { name } = s.paths[s.ix]
-  if (!name) return
-  const path = join(s.path, name)
-  getDirFiles(path).then(paths => a.show({ path, paths: filterDirs(paths) }))
-}
-
-a.jumpNext = (s, a) => {
-  const { name, dir } = s.paths[s.ix]
-  if (!dir) return
-  const path = join(s.path, name)
-  getDirFiles(path).then(paths => a.show({ path, paths: filterDirs(paths) }))
-}
-
-a.jumpPrev = (s, a) => {
-  const next = s.path.split(sep)
-  next.pop()
-  const path = join(sep, ...next)
-  getDirFiles(path).then(paths => a.show({ path, paths: filterDirs(paths) }))
-}
-
-a.show = (s, _a, { paths, path, cwd = s.cwd, create }) => ({
-  cwd, path, paths, create,
-  ix: 0,
-  val: '',
-  vis: true,
-  cache: paths,
-})
-
-// TODO: be more precise than this? also depends on scaled devices
-a.down = s => {
-  listElRef.scrollTop += 300
-  return { ix: Math.min(s.ix + 17, s.paths.length - 1) }
-}
-
-a.up = s => {
-  listElRef.scrollTop -= 300
-  return { ix: Math.max(s.ix - 17, 0) }
-}
-
-a.top = () => { listElRef.scrollTop = 0 }
-a.bottom = () => { listElRef.scrollTop = listElRef.scrollHeight }
-a.hide = () => ({ val: '', path: '', vis: false, ix: 0 })
-a.next = s => ({ ix: s.ix + 1 >= s.paths.length ? 0 : s.ix + 1 })
-a.prev = s => ({ ix: s.ix - 1 < 0 ? s.paths.length - 1 : s.ix - 1 })
-
-const ui = app({ state, view, actions: a })
+]) })
 
 const go = async (userPath: string, create = false) => {
   const cwd = await validPath(userPath) || current.cwd
