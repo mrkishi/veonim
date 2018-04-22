296d9268b2d22cafd622ea134939e02054123732
breja <gh@breja.io>
2018-04-22 11:57:44 -0400

    statusline to react

--- src/components/statusline.ts
+++ src/components/statusline.ts
@@ -1,12 +1,12 @@
 import { merge, simplifyPath, absolutePath } from '../support/utils'
 import { sub, processAnyBuffered } from '../messaging/dispatch'
 import { onStateChange, getColor } from '../core/neovim'
-import { h, app, style, Actions } from '../ui/uikit'
 import configReader from '../config/config-service'
+import { h, app, styled } from '../ui/uikit2'
 import { darken, brighten } from '../ui/css'
 import { ExtContainer } from '../core/api'
 import { colors } from '../styles/common'
-import Icon from '../components/icon'
+import Icon from '../components/icon2'
 import '../support/git'
 
 interface Tab {
@@ -19,31 +19,12 @@ interface TabInfo {
   name: string,
 }
 
-interface State {
-  tabs: TabInfo[],
-  active: number,
-  filetype: string,
-  runningServers: Set<string>,
-  erroredServers: Set<string>,
-  mode: string,
-  line: number,
-  column: number,
-  cwd: string,
-  errors: number,
-  warnings: number,
-  branch: string,
-  additions: number,
-  deletions: number,
-  macro: string,
-  baseColor: string,
-}
-
-const state: State = {
-  tabs: [],
+const state = {
+  tabs: [] as TabInfo[],
   active: -1,
   filetype: '',
-  runningServers: new Set(),
-  erroredServers: new Set(),
+  runningServers: new Set<string>(),
+  erroredServers: new Set<string>(),
   mode: 'NORMAL',
   line: 0,
   column: 0,
@@ -57,53 +38,51 @@ const state: State = {
   baseColor: '#6d576a',
 }
 
+type S = typeof state
+
 const refreshBaseColor = async () => {
   const { background } = await getColor('StatusLine')
   if (background) ui.setColor(background)
 }
 
-const Statusline = style('div')({
-  flex: 1,
-  display: 'flex',
-  justifyContent: 'space-between',
-  background: 'var(--background-30)',
-  zIndex: 999,
-})
+const Statusline = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  background: var(--background-30);
+  z-index: 999;
+`
 
-const lineGroup = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-}
+const StatusGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
 
-const Left = style('div')(lineGroup)
-const Center = style('div')(lineGroup)
-const Right = style('div')(lineGroup)
+const Item = styled.div`
+  color: var(--foreground-40);
+  display: flex;
+  height: 100%;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+`
 
-const Item = style('div')({
-  color: 'var(--foreground-40)',
-  display: 'flex',
-  height: '100%',
-  alignItems: 'center',
-  paddingLeft: '20px',
-  paddingRight: '20px',
-})
+const Tab = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  color: var(--foreground-40);
+`
 
-const Tab = style('div')({
-  display: 'flex',
-  alignItems: 'center',
-  paddingLeft: '8px',
-  paddingRight: '8px',
-  paddingTop: '4px',
-  paddingBottom: '4px',
-  color: 'var(--foreground-40)',
-})
-
-const IconBox = style('div')({
-  display: 'flex',
-  paddingRight: '4px',
-  alignItems: 'center',
-})
+const IconBox = styled.div`
+  display: flex;
+  padding-right: 4px;
+  align-items: center;
+`
 
 const container = document.getElementById('statusline') as HTMLElement
 merge(container.style, {
@@ -112,80 +91,115 @@ merge(container.style, {
   zIndex: 900,
 })
 
-const view = ({ cwd, line, column, tabs, active, filetype, runningServers, errors, warnings, branch, additions, deletions, macro, baseColor }: State) => Statusline({}, [
-  ,Left({}, [
+const actions = {
+  updateTabs: (_s: S, { active, tabs }: any) => ({ active, tabs }),
+  setFiletype: (_s: S, filetype: any) => ({ filetype }),
+  setLine: (_s: S, line: any) => ({ line }),
+  setColumn: (_s: S, column: any) => ({ column }),
+  setCwd: (_s: S, { cwd, projectRoot }: any) => ({ cwd, projectRoot }),
+  setDiagnostics: (_s: S, { errors = 0, warnings = 0 }: any) => ({ errors, warnings }),
+  setGitBranch: (_s: S, branch: any) => ({ branch }),
+  setGitStatus: (_s: S, { additions, deletions }: any) => ({ additions, deletions }),
+  setMacro: (_s: S, macro = '') => ({ macro }),
+  setColor: (_s: S, baseColor: any) => ({ baseColor }),
 
-    ,Item({
+  serverRunning: (s: S, server: any) => ({
+    runningServers: new Set([...s.runningServers, server]),
+    erroredServers: new Set([...s.erroredServers].filter(m => m !== server)),
+  }),
+
+  serverErrored: (s: S, server: any) => ({
+    runningServers: new Set([...s.runningServers].filter(m => m !== server)),
+    erroredServers: new Set([...s.erroredServers, server]),
+  }),
+
+  serverOffline: (s: S, server: any) => ({
+    runningServers: new Set([...s.runningServers].filter(m => m !== server)),
+    erroredServers: new Set([...s.erroredServers].filter(m => m !== server)),
+  }),
+}
+
+const ui = app({ name: 'statusline', state, actions, element: container, view: $ => h(Statusline, [
+
+  // LEFT
+  ,h(StatusGroup, [
+
+    ,h(Item, {
       style: {
-        color: brighten(baseColor, 90),
-        background: darken(baseColor, 20),
+        color: brighten($.baseColor, 90),
+        background: darken($.baseColor, 20),
         paddingRight: '30px',
         marginRight: '-15px',
         clipPath: 'polygon(0 0, calc(100% - 15px) 0, 100% 100%, 0 100%)',
       }
     }, [
-      ,IconBox({}, [ Icon('hard-drive') ])
-      ,h('span', cwd || 'no project')
+      ,h(IconBox, [
+        ,Icon('hardDrive')
+      ])
+
+      ,h('span', $.cwd || 'no project')
     ])
 
     // TODO: only show on git projects
-    ,Item({
+    ,h(Item, {
       style: {
         paddingLeft: '30px',
         paddingRight: '30px',
         marginRight: '-15px',
-        color: brighten(baseColor, 40),
-        background: darken(baseColor, 35),
+        color: brighten($.baseColor, 40),
+        background: darken($.baseColor, 35),
         clipPath: 'polygon(0 0, calc(100% - 15px) 0, 100% 100%, 15px 100%)',
       }
     }, [
-      ,IconBox({
-        style: { display: branch ? '' : 'none' }
-      }, [ Icon('git-branch') ])
+      ,h(IconBox, {
+        style: { display: $.branch ? '' : 'none' }
+      }, [
+        Icon('gitBranch'),
+      ])
 
-      ,h('span', branch || 'git n/a')
+      ,h('span', $.branch || 'git n/a')
     ])
 
     // TODO: only show on git projects
-    ,Item({
+    ,h(Item, {
       style: {
         paddingLeft: '30px',
         paddingRight: '30px',
         marginRight: '-15px',
-        color: brighten(baseColor, 10),
-        background: darken(baseColor, 50),
+        color: brighten($.baseColor, 10),
+        background: darken($.baseColor, 50),
         clipPath: 'polygon(0 0, calc(100% - 15px) 0, 100% 100%, 15px 100%)',
       }
     }, [
       // ADDITIONS
-      ,IconBox({
+      ,h(IconBox, {
         style: {
-          color: additions > 0 && colors.success,
+          color: $.additions > 0 && colors.success,
         }
       }, [
-        ,Icon('plus-square')
+        ,Icon('plusSquare')
       ])
 
       ,h('div', {
-        style: { color: additions > 0 && colors.success }
-      }, additions)
+        style: { color: $.additions > 0 && colors.success }
+      }, `${$.additions}`)
 
       // DELETIONS
-      ,IconBox({
+      ,h(IconBox, {
         style: {
           marginLeft: '12px',
-          color: deletions > 0 && colors.error,
+          color: $.deletions > 0 && colors.error,
         }
       }, [
-        ,Icon('minus-square')
+        ,Icon('minusSquare')
       ])
 
       ,h('div', {
-        style: { color: deletions > 0 && colors.error }
-      }, deletions)
+        style: { color: $.deletions > 0 && colors.error }
+      }, `${$.deletions}`)
     ])
 
-    ,runningServers.has(filetype) && Item({}, [
+    ,$.runningServers.has($.filetype) && h(Item, [
       ,h('div', [
         ,Icon('zap', { color: '#555' })
       ])
@@ -193,9 +207,11 @@ const view = ({ cwd, line, column, tabs, active, filetype, runningServers, error
 
   ])
 
-  ,Center({}, [
-    ,macro && Item({}, [
-      ,IconBox({
+  // CENTER
+  ,h(StatusGroup, [
+
+    ,$.macro && h(Item, [
+      ,h(IconBox, {
         style: { color: colors.error }
       }, [
         ,Icon('target')
@@ -203,109 +219,84 @@ const view = ({ cwd, line, column, tabs, active, filetype, runningServers, error
 
       ,h('div', {
         style: { color: colors.error }
-      }, macro)
+      }, $.macro)
     ])
 
   ])
 
-  ,Right({}, [
-    ,Item({
+  // RIGHT
+  ,h(StatusGroup, [
+
+    ,h(Item, {
       style: {
         paddingLeft: '30px',
         paddingRight: '30px',
-        color: brighten(baseColor, 10),
-        background: darken(baseColor, 50),
+        color: brighten($.baseColor, 10),
+        background: darken($.baseColor, 50),
         marginRight: '-15px',
         clipPath: 'polygon(15px 0, 100% 0, calc(100% - 15px) 100%, 0 100%)',
       }
     }, [
       // ERRORS
-      ,IconBox({
+      ,h(IconBox, {
         style: {
-          color: errors > 0 && colors.error,
+          color: $.errors > 0 && colors.error,
         }
       }, [
-        ,Icon('error')
+        ,Icon('xCircle')
       ])
 
       ,h('div', {
-        style: { color: errors > 0 && colors.error }
-      }, errors)
+        style: { color: $.errors > 0 && colors.error }
+      }, `${$.errors}`)
 
       // WARNINGS
-      ,IconBox({
+      ,h(IconBox, {
         style: {
           marginLeft: '12px',
-          color: warnings > 0 && colors.warning,
+          color: $.warnings > 0 && colors.warning,
         }
       }, [
-        ,Icon('warning')
+        ,Icon('alertTriangle')
       ])
 
       ,h('div', {
-        style: { color: warnings > 0 && colors.warning }
-      }, warnings)
+        style: { color: $.warnings > 0 && colors.warning }
+      }, `${$.warnings}`)
     ])
 
-    ,Item({
+    ,h(Item, {
       style: {
         paddingLeft: '30px',
         paddingRight: '20px',
-        color: brighten(baseColor, 60),
-        background: darken(baseColor, 30),
+        color: brighten($.baseColor, 60),
+        background: darken($.baseColor, 30),
         marginRight: '-20px',
         clipPath: 'polygon(15px 0, 100% 0, 100% 100%, 0 100%)',
       }
     }, [
-      ,h('div', `${line}:${column}`)
+      ,h('div', `${$.line}:${$.column}`)
     ])
 
-    ,Item({
+    ,h(Item, {
       style: {
         paddingRight: '0',
         //clipPath: 'polygon(15px 0, 100% 0, 100% 100%, 0 100%)',
       }
     }, [
-      ,tabs.map(({ id }, ix) => Tab({
+      ,$.tabs.map(({ id }, ix) => h(Tab, {
         // TODO: also display name if config declares it to
         key: id,
-        style: active === id ? { 
+        style: $.active === id ? { 
           background: 'var(--background-10)',
           color: 'var(--foreground)',
         } : undefined
       }, ix + 1))
     ])
+
   ])
-])
 
-const a: Actions<State> = {}
-a.updateTabs = (_s, _a, { active, tabs }) => ({ active, tabs })
-a.setFiletype = (_s, _a, filetype) => ({ filetype })
-a.setLine = (_s, _a, line) => ({ line })
-a.setColumn = (_s, _a, column) => ({ column })
-a.setCwd = (_s, _a, { cwd, projectRoot }) => ({ cwd, projectRoot })
-a.setDiagnostics = (_s, _a, { errors = 0, warnings = 0 }) => ({ errors, warnings })
-a.setGitBranch = (_s, _a, branch) => ({ branch })
-a.setGitStatus = (_s, _a, { additions, deletions }) => ({ additions, deletions })
-a.setMacro = (_s, _a, macro = '') => ({ macro })
-a.setColor = (_s, _a, baseColor) => ({ baseColor })
-
-a.serverRunning = (s, _a, server) => ({
-  runningServers: new Set([...s.runningServers, server]),
-  erroredServers: new Set([...s.erroredServers].filter(m => m !== server)),
-})
-
-a.serverErrored = (s, _a, server) => ({
-  runningServers: new Set([...s.runningServers].filter(m => m !== server)),
-  erroredServers: new Set([...s.erroredServers, server]),
-})
-
-a.serverOffline = (s, _a, server) => ({
-  runningServers: new Set([...s.runningServers].filter(m => m !== server)),
-  erroredServers: new Set([...s.erroredServers].filter(m => m !== server)),
-})
-
-const ui = app({ state, view, actions: a }, false, container)
+]) })
 
 sub('colorscheme.modified', refreshBaseColor)
 onStateChange.colorscheme(refreshBaseColor)
