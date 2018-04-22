22012fe3c700c5afa130b2dc38d082372f934c21
breja <gh@breja.io>
2018-04-21 22:20:26 -0400

    grep to react

--- src/components/grep.ts
+++ src/components/grep.ts
@@ -1,8 +1,10 @@
-import { action, cmd, call, current, feedkeys, expr } from '../core/neovim'
-import { h, app, Actions, ActionCaller } from '../ui/uikit'
-import { Plugin, Row, Badge } from '../styles/common'
-import Input from '../components/text-input'
+import { action, call, current, feedkeys, expr, jumpTo } from '../core/neovim'
+import { RowNormal, RowHeader } from '../components/row-container'
+import { PluginRight } from '../components/plugin-container'
+import Input from '../components/text-input2'
+import { h, app, styled } from '../ui/uikit2'
 import Worker from '../messaging/worker'
+import Badge from '../components/badge'
 
 type TextTransformer = (text: string, last?: boolean) => string
 type Result = [string, SearchResult[]]
@@ -18,34 +20,29 @@ interface SearchResult {
   text: string,
 }
 
-interface State {
-  val: string,
-  filterVal: string,
-  cwd: string,
-  results: Result[],
-  vis: boolean,
-  ix: number,
-  subix: number,
-  loading: boolean,
-  focused: FocusedElement,
-}
+const RowGroup = styled.div`
+  paddingTop: 4px;
+  paddingBottom: 4px;
+`
 
 let elref: HTMLElement
 const SCROLL_AMOUNT = 0.25
 const worker = Worker('search-files')
 const els = new Map<number, HTMLElement>()
 
-const state: State = {
-  val: '',
+const state = {
+  value: '',
   filterVal: '',
   cwd: '',
-  results: [],
-  vis: false,
+  results: [] as Result[],
+  visible: false,
   ix: 0,
   subix: -1,
   loading: false,
   focused: FocusedElement.Search,
 }
+
+type S = typeof state
 
 // scroll after next section has been rendered as expanded (a little hacky)
 const scrollIntoView = (next: number) => setTimeout(() => {
@@ -68,14 +65,8 @@ const scrollIntoView = (next: number) => setTimeout(() => {
 const selectResult = (results: Result[], ix: number, subix: number) => {
   if (subix < 0) return
   const [ path, items ] = results[ix]
-  const { line } = items[subix]
-  openResult(path, line)
-}
-
-// TODO: use neovim.jumpTo
-const openResult = (path: string, line: number) => {
-  cmd(`e ${path}`)
-  feedkeys(`${line}G`)
+  const { line, col: column } = items[subix]
+  jumpTo({ path, line, column })
 }
 
 const highlightPattern = (text: string, pattern: string, { normal, special }: {
@@ -95,20 +86,104 @@ const highlightPattern = (text: string, pattern: string, { normal, special }: {
     }, [] as string[])
 }
 
-const view = ($: State, actions: ActionCaller) => Plugin.right('grep', $.vis, [
+const resetState = { visible: false, loading: false }
+
+const actions = {
+  focusSearch: () => ({ focused: FocusedElement.Search }),
+  focusFilter: () => ({ focused: FocusedElement.Filter }),
+
+  hide: () => resetState,
+  show: (_s: S, { cwd, value, reset = true }: any) => reset
+  ? ({ visible: true, cwd, value, ix: 0, subix: -1, results: [], loading: !!value })
+  : ({ visible: true }),
+
+  select: (s: S) => {
+    if (!s.results.length) return resetState
+    selectResult(s.results, s.ix, s.subix)
+    return resetState
+  },
+
+  change: (s: S, value: string) => {
+    value && worker.call.query({ query: value, cwd: s.cwd })
+    return value ? {
+      value,
+      loading: true,
+    } : {
+      value,
+      results: [],
+      ix: 0,
+      subix: 0,
+      loading: false,
+    }
+  },
+
+  changeFilter: (_s: S, filterVal: string) => {
+    worker.call.filter(filterVal)
+    return { filterVal }
+  },
+
+  results: (_s: S, results: Result[]) => ({ results }),
+
+  moreResults: (s: S, results: Result[]) => {
+    const merged = [ ...s.results, ...results ]
+    const deduped = merged.filter((m, ix, arr) => arr.findIndex(e => e[0] === m[0]) === ix)
+    return { results: deduped }
+  },
+
+  nextGroup: (s: S) => {
+    const next = s.ix + 1 > s.results.length - 1 ? 0 : s.ix + 1
+    scrollIntoView(next)
+    return { subix: -1, ix: next }
+  },
+
+  prevGroup: (s: S) => {
+    const next = s.ix - 1 < 0 ? s.results.length - 1 : s.ix - 1
+    scrollIntoView(next)
+    return { subix: -1, ix: next }
+  },
+
+  next: (s: S) => {
+    const next = s.subix + 1 < s.results[s.ix][1].length ? s.subix + 1 : 0
+    selectResult(s.results, s.ix, next)
+    return { subix: next }
+  },
+
+  prev: (s: S) => {
+    const prev = s.subix - 1 < 0 ? s.results[s.ix][1].length - 1 : s.subix - 1
+    selectResult(s.results, s.ix, prev)
+    return { subix: prev }
+  },
+
+  down: () => {
+    const { height } = elref.getBoundingClientRect()
+    const maxScroll = elref.scrollHeight - height
+    // TODO: should wait until get results back before calling loadNext again...
+    if (elref.scrollTop === maxScroll) return worker.call.loadNext()
+    elref.scrollTop += Math.floor(height * SCROLL_AMOUNT)
+  },
+
+  up: () => {
+    const { height } = elref.getBoundingClientRect()
+    elref.scrollTop -= Math.floor(height * SCROLL_AMOUNT)
+  },
+
+  loadingDone: () => ({ loading: false }),
+}
+
+const ui = app({ name: 'grep', state, actions, view: ($, a) => PluginRight($.visible, [
 
   ,Input({
-    val: $.val,
-    change: actions.change,
-    hide: actions.hide,
-    tab: actions.focusFilter,
-    select: actions.select,
-    nextGroup: actions.nextGroup,
-    prevGroup: actions.prevGroup,
-    next: actions.next,
-    prev: actions.prev,
-    down: actions.down,
-    up: actions.up,
+    value: $.value,
+    change: a.change,
+    hide: a.hide,
+    tab: a.focusFilter,
+    select: a.select,
+    nextGroup: a.nextGroup,
+    prevGroup: a.prevGroup,
+    next: a.next,
+    prev: a.prev,
+    down: a.down,
+    up: a.up,
     focus: $.focused === FocusedElement.Search,
     icon: 'search',
     desc: 'find in project',
@@ -116,17 +191,17 @@ const view = ($: State, actions: ActionCaller) => Plugin.right('grep', $.vis, [
   }),
 
   ,Input({
-    val: $.filterVal,
-    change: actions.changeFilter,
-    hide: actions.hide,
-    tab: actions.focusSearch,
-    select: actions.select,
-    nextGroup: actions.nextGroup,
-    prevGroup: actions.prevGroup,
-    next: actions.next,
-    prev: actions.prev,
-    down: actions.down,
-    up: actions.up,
+    value: $.filterVal,
+    change: a.changeFilter,
+    hide: a.hide,
+    tab: a.focusSearch,
+    select: a.select,
+    nextGroup: a.nextGroup,
+    prevGroup: a.prevGroup,
+    next: a.next,
+    prev: a.prev,
+    down: a.down,
+    up: a.up,
     focus: $.focused === FocusedElement.Filter,
     icon: 'filter',
     small: true,
@@ -135,127 +210,58 @@ const view = ($: State, actions: ActionCaller) => Plugin.right('grep', $.vis, [
 
   // TODO: render keys? idk about keys they seem to not work like in react...
   ,h('div', {
-    onupdate: (e: HTMLElement) => elref = e,
+    ref: (e: HTMLElement) => {
+      if (e) elref = e
+    },
     style: {
       maxHeight: '100%',
       overflowY: 'hidden',
     },
   }, $.results.map(([ path, items ], pos) => h('div', {
-    oncreate: (e: HTMLElement) => els.set(pos, e),
+    ref: (e: HTMLElement) => {
+      if (e) els.set(pos, e)
+    },
   }, [
 
-    ,Row.header({ activeWhen: pos === $.ix }, [
+    ,h(RowHeader, {
+      active: pos === $.ix,
+    }, [
       ,h('span', path),
-      ,Badge(items.length, { marginLeft: '12px' })
+      ,h(Badge, {
+        style: { marginLeft: '12px' },
+      }, [
+        ,h('span', items.length)
+      ])
     ])
 
-    ,pos === $.ix && Row.group({}, items.map((f, itemPos) => Row.normal({
-      activeWhen: pos === $.ix && itemPos === $.subix
-    }, highlightPattern(f.text, $.val, {
+    ,pos === $.ix && h(RowGroup, {}, items.map((f, itemPos) => h(RowNormal, {
+      key: `${f.line}-${f.col}-${f.text}`,
+      active: pos === $.ix && itemPos === $.subix,
+    }, [
+      ,h('span', highlightPattern(f.text, $.value, {
 
-      normal: (text, last) => h('span', {
-        style: {
-          whiteSpace: 'pre',
-          textOverflow: last ? 'ellipsis' : undefined,
-          overflow: last ? 'inherit' : undefined,
-        },
-      }, text),
+        normal: (text, last) => h('span', {
+          style: {
+            whiteSpace: 'pre',
+            textOverflow: last ? 'ellipsis' : undefined,
+            overflow: last ? 'inherit' : undefined,
+          },
+        }, text),
 
-      special: text => h('span.highlight', {
-        style: {
-          color: '#aaa',
-          background: 'rgba(255, 255, 255, 0.1)',
-        }
-      }, text),
-    }))))
+        special: text => h('span.highlight', {
+          style: {
+            color: '#aaa',
+            background: 'rgba(255, 255, 255, 0.1)',
+          }
+        }, text)
+
+      }))
+    ])))
 
   ])))
 
-])
+]) })
 
-const a: Actions<State> = {}
-
-a.focusSearch = () => ({ focused: FocusedElement.Search })
-a.focusFilter = () => ({ focused: FocusedElement.Filter })
-
-a.hide = () => ({ vis: false, loading: false })
-a.show = (_s, _a, { cwd, val, reset = true }) => reset
-  ? ({ vis: true, cwd, val, ix: 0, subix: -1, results: [], loading: !!val })
-  : ({ vis: true })
-
-a.select = (s, a) => {
-  if (!s.results.length) return a.hide()
-  selectResult(s.results, s.ix, s.subix)
-  a.hide()
-}
-
-a.change = (s, _a, val: string) => {
-  val && worker.call.query({ query: val, cwd: s.cwd })
-  return val ? {
-    val,
-    loading: true,
-  } : {
-    val,
-    results: [],
-    ix: 0,
-    subix: 0,
-    loading: false,
-  }
-}
-
-a.changeFilter = (_s, _a, filterVal: string) => {
-  worker.call.filter(filterVal)
-  return { filterVal }
-}
-
-a.results = (_s, _a, results: Result[]) => ({ results })
-
-a.moreResults = (s, _a, results: Result[]) => {
-  const merged = [ ...s.results, ...results ]
-  const deduped = merged.filter((m, ix, arr) => arr.findIndex(e => e[0] === m[0]) === ix)
-  return { results: deduped }
-}
-
-a.nextGroup = s => {
-  const next = s.ix + 1 > s.results.length - 1 ? 0 : s.ix + 1
-  scrollIntoView(next)
-  return { subix: -1, ix: next }
-}
-
-a.prevGroup = s => {
-  const next = s.ix - 1 < 0 ? s.results.length - 1 : s.ix - 1
-  scrollIntoView(next)
-  return { subix: -1, ix: next }
-}
-
-a.next = s => {
-  const next = s.subix + 1 < s.results[s.ix][1].length ? s.subix + 1 : 0
-  selectResult(s.results, s.ix, next)
-  return { subix: next }
-}
-
-a.prev = s => {
-  const prev = s.subix - 1 < 0 ? s.results[s.ix][1].length - 1 : s.subix - 1
-  selectResult(s.results, s.ix, prev)
-  return { subix: prev }
-}
-
-a.down = () => {
-  const { height } = elref.getBoundingClientRect()
-  const maxScroll = elref.scrollHeight - height
-  // TODO: should wait until get results back before calling loadNext again...
-  if (elref.scrollTop === maxScroll) return worker.call.loadNext()
-  elref.scrollTop += Math.floor(height * SCROLL_AMOUNT)
-}
-
-a.up = () => {
-  const { height } = elref.getBoundingClientRect()
-  elref.scrollTop -= Math.floor(height * SCROLL_AMOUNT)
-}
-
-a.loadingDone = () => ({ loading: false })
-
-const ui = app({ state, view, actions: a })
 worker.on.results((results: Result[]) => ui.results(results))
 worker.on.moreResults((results: Result[]) => ui.moreResults(results))
 worker.on.done(ui.loadingDone)
@@ -271,7 +277,7 @@ action('grep', async (query: string) => {
 action('grep-word', async () => {
   const { cwd } = current
   const query = await call.expand('<cword>')
-  ui.show({ cwd, val: query })
+  ui.show({ cwd, value: query })
   worker.call.query({ query, cwd })
 })
 
@@ -280,6 +286,6 @@ action('grep-selection', async () => {
   const selection = await expr('@z')
   const [ query ] = selection.split('\n')
   const { cwd } = current
-  ui.show({ cwd, val: query })
+  ui.show({ cwd, value: query })
   worker.call.query({ query, cwd })
 })
