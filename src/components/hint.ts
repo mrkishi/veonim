8dc8db87f53ab8611e71f3bba6f9cbe9c8298956
breja <gh@breja.io>
2018-04-26 22:05:48 -0400

    wont you take a hint

--- src/components/hint.ts
+++ src/components/hint.ts
@@ -1,27 +1,98 @@
-import { connect } from '../state/trade-federation'
-import { docStyle } from '../styles/common'
 import { activeWindow } from '../core/windows'
-import Overlay from '../components/overlay2'
-import { h, styled } from '../ui/uikit2'
-import { Hint } from '../state/hint'
+import Overlay from '../components/overlay'
+import { docStyle } from '../styles/common'
+import { h, app } from '../ui/uikit'
 import { cvar } from '../ui/css'
 
-const Faded = styled.span`
-  color: var(--foreground);
-  filter: opacity(60%);
-`
+interface ShowParams {
+  row: number,
+  col: number,
+  label: string,
+  currentParam: string,
+  documentation?: string,
+  paramDoc?: string,
+  totalSignatures: number,
+  selectedSignature: number,
+}
 
-const Strong = styled.span`
-  color: var(--foreground);
-  font-weight: bold;
-`
+const fadedStyle = {
+  color: cvar('foreground'),
+  filter: 'opacity(60%)',
+}
+
+const strongStyle = {
+  color: cvar('foreground'),
+  fontWeight: 'bold',
+}
 
 const docs = (data: string) => h('div', {
   style: docStyle,
 }, [ h('div', data) ])
 
-const view = ({ data: $ }: { data: Hint }) => Overlay({
-  name: 'hint',
+const sliceAndDiceLabel = (label: string, currentParam: string) => {
+  const paramStart = label.indexOf(currentParam)
+  const labelStart = label.slice(0, paramStart)
+  const activeParam = label.slice(paramStart, paramStart + currentParam.length)
+  const labelEnd = label.slice(paramStart + currentParam.length)
+  return { labelStart, labelEnd, activeParam }
+}
+
+const fresh = ({ row, col, documentation, selectedSignature, totalSignatures }: any) => ({
+  row,
+  col,
+  documentation,
+  selectedSignature,
+  totalSignatures,
+})
+
+const state = {
+  label: '',
+  labelStart: '',
+  labelEnd: '',
+  currentParam: '',
+  documentation: '',
+  paramDoc: '',
+  anchorBottom: true,
+  totalSignatures: 0,
+  selectedSignature: 0,
+  visible: false,
+  row: 0,
+  col: 0,
+}
+
+type S = typeof state
+
+const actions = {
+  hide: () => ({ visible: false, label: '', row: 0 }),
+  show: ({
+    row,
+    col,
+    label,
+    currentParam,
+    documentation,
+    paramDoc,
+    selectedSignature,
+    totalSignatures,
+  }: ShowParams) => (s: S) => {
+    const { labelStart, labelEnd, activeParam } = sliceAndDiceLabel(label, currentParam)
+    const same = s.label === label && s.row === row
+    const stuff = same ? {} : fresh({ row, col, documentation, selectedSignature, totalSignatures })
+
+    return {
+      ...stuff,
+      label,
+      labelStart,
+      labelEnd,
+      paramDoc,
+      anchorBottom: row > 2,
+      currentParam: activeParam,
+      visible: true,
+    }
+
+  },
+}
+
+const view = ($: S) => Overlay({
   x: activeWindow() ? activeWindow()!.colToX($.col - 1) : 0,
   y: activeWindow() ? activeWindow()!.rowToTransformY($.row > 2 ? $.row : $.row + 1) : 0,
   zIndex: 200,
@@ -48,9 +119,9 @@ const view = ({ data: $ }: { data: Hint }) => Overlay({
       padding: '8px',
     } }, [
       ,h('div', [
-        ,h(Faded, [ h('span', $.labelStart) ])
-        ,h(Strong, [ h('span', $.currentParam) ])
-        ,h(Faded, [ h('span', $.labelEnd) ])
+        ,h('div', { style: fadedStyle }, [ h('span', $.labelStart) ])
+        ,h('div', { style: strongStyle }, [ h('span', $.currentParam) ])
+        ,h('div', { style: fadedStyle }, [ h('span', $.labelEnd) ])
       ])
 
       ,h('div', {
@@ -65,4 +136,4 @@ const view = ({ data: $ }: { data: Hint }) => Overlay({
 
 ])
 
-export default connect(s => ({ data: s.hint }))(view)
+app({ name: 'hint', state, actions, view })
