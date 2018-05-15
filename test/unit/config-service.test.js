e93e8081a324c7da6b0b6b16bd0e8bfd085e772e
breja <gh@breja.io>
2018-05-14 21:45:24 -0400

    idk more tests maybe

---
+++ test/unit/config-service.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { src, same } = require('./util')
+
+const defaultConfig = {
+  project: {
+    root: '~/blarg'
+  }
+}
+
+const setup = vimConfig => src('config/config-service', {
+  'config/default-configs': defaultConfig,
+  'core/neovim': {
+    g: vimConfig,
+    '@noCallThru': true,
+  }
+}).default
+
+describe('config service', () => {
+  it('do the needful', done => {
+    const cs = setup({ 'vn_project_root': Promise.resolve('shittake') })
+
+    const val = cs('project.root', m => {
+      same(m, 'shittake')
+      done()
+    })
+
+    same(val, '~/blarg')
+  })
+})
