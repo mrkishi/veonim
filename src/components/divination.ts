ce100f33eb25410d9616cd8a995aefaddbe1dbf6
breja <gh@breja.io>
2018-06-15 21:25:25 -0400

    improve styling of divination

--- src/components/divination.ts
+++ src/components/divination.ts
@@ -1,13 +1,13 @@
 import { getWindowContainerElement, activeWindow } from '../core/windows'
+import { genList, merge } from '../support/utils'
 import { action, feedkeys } from '../core/neovim'
 import { cell } from '../core/canvas-container'
-import { genList } from '../support/utils'
 import { cursor } from '../core/cursor'
 import * as input from '../core/input'
 import { makel } from '../ui/vanilla'
 import { paddingV } from '../ui/css'
 
-const jumpKeys = 'asdflghqwertyuiopbnmcbvjk'
+const jumpKeys = 'ASDFLGHQWERTYUIOPBNMCBVJK'
 
 const jumpLabels = jumpKeys.split('').map(key => {
   const otherKeys = jumpKeys.replace(key, '')
@@ -39,12 +39,21 @@ action('divination', () => {
     })
 
     const label = jumpLabels[ix]
-    el.innerHTML = `<span>${label[0]}</span><span>${label[1]}</span>`
+    // using margin-right instead of letter-spacing because letter-spacing adds space
+    // to the right of the last letter - so it ends up with more padding on the right :/
+    el.innerHTML = `<span style="margin-right: 2px">${label[0]}</span><span>${label[1]}</span>`
     return el
   })
 
   labels.forEach(label => labelContainer.appendChild(label))
   winContainer.appendChild(labelContainer)
+
+  const updateLabels = (matchChar: string) => labels
+    .filter(m => (m.children[0] as HTMLElement).innerText.toLowerCase() === matchChar)
+    .forEach(m => merge((m.children[0] as HTMLElement).style, {
+      // TODO: inherit from colorscheme
+      color: '#ff007c'
+    }))
 
   input.blur()
   const grabbedKeys: string[] = []
@@ -64,6 +73,7 @@ action('divination', () => {
 
   const keyHandler = ({ key }: KeyboardEvent) => {
     grabbedKeys.push(key)
+    if (grabbedKeys.length === 1) return updateLabels(key)
     if (grabbedKeys.length === 2) joinTheDarkSide()
   }
 
