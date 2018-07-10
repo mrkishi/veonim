61a0b90e3893399ce0ce72ca990169e9169ee488
breja <gh@breja.io>
2018-07-09 20:45:00 -0400

    improved window titles

--- src/core/window-metadata.ts
+++ src/core/window-metadata.ts
@@ -1,9 +1,10 @@
 import { current, getCurrent, BufferOption, BufferType } from '../core/neovim'
+import { simplifyPath, pathReducer } from '../support/utils'
 import { BufferVar } from '../core/vim-functions'
-import { simplifyPath } from '../support/utils'
 
 export interface WindowMetadata {
   id: number
+  dir: string
   name: string
   filetype: string
   active: boolean
@@ -13,12 +14,31 @@ export interface WindowMetadata {
   termFormat: string
 }
 
+const improvedWindowTitle = (name: string, uniqNames: Set<string>, terminal: boolean) => {
+  if (terminal || !name) return { name }
+
+  const uniqueNames = [...uniqNames].filter(m => m !== name)
+  const uniqueNameReducers = uniqueNames.map(m => pathReducer(m))
+  const nameReducer = pathReducer(name)
+
+  const file = nameReducer.reduce()
+  const uniqueFileNames = new Set(uniqueNameReducers.map(m => m.reduce()))
+
+  // TODO: go n-levels deeper
+  return { name: file, dir: uniqueFileNames.has(file) ? nameReducer.reduce() : undefined }
+}
+
+const betterTitles = (windows: any[]): WindowMetadata[] => {
+  const uniqNames = new Set(windows.map(w => w.name))
+  return windows.map(w => ({ ...w, ...improvedWindowTitle(w.name, uniqNames, w.terminal) }))
+}
+
 export default async (): Promise<WindowMetadata[]> => {
   // TODO: fix neovim.current to handle fuctions AND properties, then use that here
   const activeWindow = (await getCurrent.window).id
   const wins = await (await getCurrent.tab).windows
 
-  return Promise.all(wins.map(async w => {
+  const windowsWithApiData = await Promise.all(wins.map(async w => {
     const buffer = await w.buffer
 
     return {
@@ -32,4 +52,6 @@ export default async (): Promise<WindowMetadata[]> => {
       termFormat: await buffer.getVar(BufferVar.TermFormat).catch(() => ''),
     }
   }))
+
+  return betterTitles(windowsWithApiData)
 }
