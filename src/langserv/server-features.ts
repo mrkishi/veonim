7fd079f4ace49e1117dff71dde5dbed1d24883a2
breja <gh@breja.io>
2018-07-12 21:04:05 -0400

    this is probably the right way to go about this

--- src/langserv/server-features.ts
+++ src/langserv/server-features.ts
@@ -1,27 +1,33 @@
+import { ServerCapabilities } from 'vscode-languageserver-protocol'
 import toVSCodeLanguage from '../langserv/vsc-languages'
 import { onFnCall } from '../support/utils'
 
-export enum ServerFeature {
-  Completion = 'completion',
-  CompletionDetail = 'completionDetail',
-  Definition = 'definition',
-  Hover = 'hover',
-  SignatureHint = 'signatureHint',
-  Highlights = 'highlights',
-  References = 'references',
-  Rename = 'rename',
-  Symbols = 'symbols',
-  WorkspaceSymbols = 'workspaceSymbols',
-  Diagnostics = 'diagnostics',
+type EnableCheckFn = (cwd: string, filetype: string) => void
+
+export interface ServerFeatures {
+  completion: EnableCheckFn
+  completionDetail: EnableCheckFn
+  definition: EnableCheckFn
+  hover: EnableCheckFn
+  signatureHint: EnableCheckFn
+  highlights: EnableCheckFn
+  references: EnableCheckFn
+  rename: EnableCheckFn
+  symbols: EnableCheckFn
+  workspaceSymbols: EnableCheckFn
+  diagnostics: EnableCheckFn
 }
 
-const servers = new Map<string, Map<ServerFeature, boolean>>()
+type Feature = keyof ServerFeatures
 
-const capabilitiesToFeatures = (capabilities: any) => {
+const servers = new Map<string, Map<Feature, boolean>>()
 
+const capabilitiesToFeatures = (capabilities: ServerCapabilities) => {
+  // TODO: parse capabilities into Map
+  return new Map()
 }
 
-export const registerServer = (cwd: string, language: string, capabilities: any) => {
+export const registerServer = (cwd: string, language: string, capabilities: ServerCapabilities) => {
   servers.set(cwd + language, capabilitiesToFeatures(capabilities))
 }
 
@@ -29,16 +35,14 @@ export const unregisterServer = (cwd: string, language: string) => {
   servers.delete(cwd + language)
 }
 
-const featureEnabled = (cwd: string, filetype: string, feature: ServerFeature): boolean => {
+const featureEnabled = (cwd: string, filetype: string, feature: Feature): boolean => {
   const language = toVSCodeLanguage(filetype)
   const serverFeatures = servers.get(cwd + language)
   if (!serverFeatures) return false
   return !!serverFeatures.get(feature)
 }
 
-type isEnabledProxy = { [index: keyof ServerFeature]: (cwd: string, filetype: string) => boolean }
-
-export const isEnabled = <isEnabledProxy>onFnCall((feature: ServerFeature, ...args: any[]) => {
+export const serverSupports = <ServerFeatures>onFnCall((name, ...args: any[]) => {
   const [ cwd, filetype ] = args
-  return featureEnabled(cwd, filetype, feature)
+  return featureEnabled(cwd, filetype, name as Feature)
 })
