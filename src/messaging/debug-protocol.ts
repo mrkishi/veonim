39c9788d0f28faace6a4d81b2401a3f7dea79dd4
breja <gh@breja.io>
2018-07-13 23:41:32 -0400

    i think we have a debug protocol api now yippie

--- src/messaging/debug-protocol.ts
+++ src/messaging/debug-protocol.ts
@@ -1,14 +1,56 @@
-// ref: vscode/blob/master/src/vs/workbench/parts/debug/node/debugAdapter.ts
 import { DebugProtocol as DP } from 'vscode-debugprotocol'
-import { ID, Watchers } from '../support/utils'
 import { Readable, Writable } from 'stream'
+import { ID } from '../support/utils'
 import {} from 'vscode-debug'
+
+export default (readable: Readable, writable: Writable) => {
+  const pendingRequests = new Map()
+  const id = ID()
+
+  let onErrorFn = (_: any) => {}
+  let onEventFn = (_: DP.Event) => {}
+  let onRequestFn = (_: DP.Request) => {}
+
+  const onMessage = (msg: DP.ProtocolMessage) => {
+    if (msg.type === 'event') return onEventFn(msg as DP.Event)
+    if (msg.type === 'request') return onRequestFn(msg as DP.Request)
+    if (msg.type === 'request') {
+      const m = (msg as DP.Response)
+      if (!pendingRequests.has(m.request_seq)) return
+
+      const { done, fail } = pendingRequests.get(m.request_seq)
+      m.success ? done(m) : fail(m)
+      pendingRequests.delete(m.request_seq)
+    }
+  }
+
+  const sendResponse = (response: DP.Response) => {
+    if (response.seq > 0) return onErrorFn(new Error(`don't send more than one response for: ${response.command}`))
+    const seq = id.next()
+    connection.send({ seq, type: 'response', command: response.command })
+  }
+
+  const sendRequest = (command: string, args: any) => {
+    const seq = id.next()
+    connection.send({ command, seq, type: 'request', arguments: args })
+    return new Promise((done, fail) => pendingRequests.set(seq, { done, fail }))
+  }
+
+  const onEvent = (cb: (event: DP.Event) => void) => onEventFn = cb
+  const onRequest = (cb: (request: DP.Request) => void) => onRequestFn = cb
+  const onError = (cb: (error: any) => void) => onErrorFn = cb
+
+  const connection = streamProcessor(readable, writable, onMessage, onErrorFn)
+
+  return { sendRequest, sendResponse, onEvent, onRequest, onError }
+}
 
 const TWO_CRLF = '\r\n\r\n'
 const HEADER_LINE_SEP = /\r?\n/
 const HEADER_FIELD_SEP = /: */
 const TWO_CRLF_LENGTH = TWO_CRLF.length
 
+// stolen from: vscode/blob/master/src/vs/workbench/parts/debug/node/debugAdapter.ts
 const streamProcessor = (readable: Readable, writable: Writable, onMessage: Function, onError: Function) => {
   let rawData = Buffer.allocUnsafe(0)
   let contentLength = -1
@@ -58,25 +100,12 @@ const streamProcessor = (readable: Readable, writable: Writable, onMessage: Func
     }
   })
 
-  const send = (message: DP.ProtocolMessage) => {
-    if (!writable || !writable.write) return
+  const send = <T extends DP.ProtocolMessage>(message: T) => {
+    if (!writable) return
     const json = JSON.stringify(message)
     const length = Buffer.byteLength(json, 'utf8')
     writable.write(`Content-Length: ${length}${TWO_CRLF}${json}`, 'utf8')
   }
 
   return { send }
-}
-
-export default (readable: Readable, writable: Writable) => {
-  const requestHandlers = new Map<string, Function>()
-  const pendingRequests = new Map()
-  const id = ID()
-  const onError = () => {}
-  const onMessage = () => {}
-  const connection = streamProcessor(readable, writable, onMessage, onError)
-
-  const request = (command: string, ...args: any[]) => {
-    const reqId = id.next()
-  }
 }
