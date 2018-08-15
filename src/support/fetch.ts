3d160d001b8c24d100fedaa2b9e8e5cc77980e9a
breja <gh@breja.io>
2018-08-15 16:23:23 -0400

    add typings to fetchStream

--- src/support/fetch.ts
+++ src/support/fetch.ts
@@ -1,10 +1,15 @@
-// TODO: type the request options
-export const stream = (url: string, options = { method: 'GET' }) => new Promise((done, fail) => {
+import { RequestOptions, IncomingMessage } from 'http'
+
+interface FetchOptions extends RequestOptions {
+  data?: any
+}
+
+export const fetchStream = (url: string, options = { method: 'GET' } as FetchOptions) => new Promise((done, fail) => {
   const { data, ...requestOptions } = options
   const opts = { ...require('url').parse(url), ...requestOptions }
 
-  const req = require(url.startsWith('https://') ? 'https' : 'http').request(opts, res => done(res.statusCode >= 300 && res.statusCode < 400
-    ? stream(res.headers.location, options)
+  const req = require(url.startsWith('https://') ? 'https' : 'http').request(opts, (res: IncomingMessage) => done(res.statusCode! >= 300 && res.statusCode! < 400
+    ? fetchStream(res.headers.location!, options)
     : res))
 
   req.on('error', fail)
