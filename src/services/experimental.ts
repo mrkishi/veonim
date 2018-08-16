cc33a60474944efd9784ac2c59b66f7d9cb5c575
breja <gh@breja.io>
2018-08-15 22:06:20 -0400

    capture and replay events

--- src/services/experimental.ts
+++ src/services/experimental.ts
@@ -14,3 +14,51 @@ action('derp:explorer', async () => {
   cmd('vert resize 30')
   cmd('b Explorer')
 })
+
+const monitorEvents = [
+  'keydown',
+  'keyup',
+  'keypress',
+  'input',
+  'beforeinput',
+  'change',
+  'focus',
+  'blur',
+  'resize',
+]
+
+let recordedEvents = [] as any[]
+let captureEvents = true
+let startTime = Date.now()
+console.log('starting to capture for 10s')
+
+monitorEvents.forEach(ev => window.addEventListener(ev, e => {
+  if (!captureEvents) return
+  console.log(ev, e)
+  recordedEvents.push([ Date.now() - startTime, e ])
+}))
+
+setTimeout(() => {
+  console.log('INSTANT REPLAY')
+  captureEvents = false
+  console.log(recordedEvents)
+  recordedEvents.forEach(([ time, ev ]) => {
+    setTimeout(() => {
+      ev.target.dispatchEvent(ev)
+    }, time)
+  })
+  recordedEvents = []
+  captureEvents = true
+}, 10e3)
+
+// action('replay', () => {
+//   captureEvents = false
+//   recordedEvents.forEach(e => window.dispatchEvent(e))
+//   recordedEvents = []
+//   captureEvents = true
+// })
+
+// discover all events
+// Object.keys(window).forEach(key => {
+//   if (/on/.test(key)) console.log(key.slice(2))
+// })
