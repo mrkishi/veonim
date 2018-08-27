ccbeff548195196ad5c6b60452463e0fbbec44b9
breja <gh@breja.io>
2018-08-27 16:35:58 -0400

    code quality

--- src/messaging/dispatch.ts
+++ src/messaging/dispatch.ts
@@ -2,7 +2,9 @@ import { Watchers } from '../support/utils'
 
 const watchers = new Watchers()
 const cache = new Map<string, any[]>()
-const buffer = (event: string, data: any) => cache.has(event) ? cache.get(event)!.push(data) : cache.set(event, [data])
+const buffer = (event: string, data: any) => cache.has(event)
+  ? cache.get(event)!.push(data)
+  : cache.set(event, [data])
 
 export const unsub = (event: string, cb: Function) => watchers.remove(event, cb)
 export const sub = (event: string, cb: (...args: any[]) => void) => watchers.add(event, cb)
