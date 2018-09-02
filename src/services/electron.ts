7045792f7af3b19236175b732fb9f1b19d8a5bce
breja <gh@breja.io>
2018-09-02 19:47:29 -0400

    display veonim version within app - fix #471

--- src/services/electron.ts
+++ src/services/electron.ts
@@ -1,6 +1,7 @@
 import nvim from '../core/neovim'
 import { remote } from 'electron'
 
+nvim.onAction('version', () => nvim.cmd(`echo 'Veonim v${remote.app.getVersion()}'`))
 nvim.onAction('hide', () => remote.app.hide())
 nvim.onAction('quit', () => remote.app.quit())
 nvim.onAction('maximize', () => remote.getCurrentWindow().maximize())
