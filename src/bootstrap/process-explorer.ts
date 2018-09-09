9e6a29ee9b2f298f6ad98a5d7a7deaf10c53ab30
breja <gh@breja.io>
2018-09-09 19:02:06 -0400

    process explorer: more cmds parsings

--- src/bootstrap/process-explorer.ts
+++ src/bootstrap/process-explorer.ts
@@ -56,57 +56,6 @@ const listProcesses = (rootPid: number): Promise<ProcessItem> => new Promise(don
     }
   }
 
-  //function findName(cmd: string): string {
-  //  const SHARED_PROCESS_HINT = /--disable-blink-features=Auxclick/
-  //  const WINDOWS_WATCHER_HINT = /\\watcher\\win32\\CodeHelper\.exe/
-  //  const WINDOWS_CRASH_REPORTER = /--crashes-directory/
-  //  const WINDOWS_PTY = /\\pipe\\winpty-control/
-  //  const WINDOWS_CONSOLE_HOST = /conhost\.exe/
-  //  const TYPE = /--type=([a-zA-Z-]+)/
-
-  //  // find windows file watcher
-  //  if (WINDOWS_WATCHER_HINT.exec(cmd)) return 'watcherService '
-
-  //  // find windows crash reporter
-  //  if (WINDOWS_CRASH_REPORTER.exec(cmd)) return 'electron-crash-reporter'
-
-  //  // find windows pty process
-  //  if (WINDOWS_PTY.exec(cmd)) return 'winpty-process'
-
-  //  //find windows console host process
-  //  if (WINDOWS_CONSOLE_HOST.exec(cmd)) return 'console-window-host (Windows internal process)'
-
-  //  // find "--type=xxxx"
-  //  let matches = TYPE.exec(cmd)
-  //  if (matches && matches.length === 2) {
-  //    if (matches[1] === 'renderer') {
-  //      if (SHARED_PROCESS_HINT.exec(cmd)) {
-  //        return 'shared-process'
-  //      }
-
-  //      return `window`
-  //    }
-  //    return matches[1]
-  //  }
-
-  //  // find all xxxx.js
-  //  const JS = /[a-zA-Z-]+\.js/g
-  //  let result = ''
-  //  do {
-  //    matches = JS.exec(cmd)
-  //    if (matches) {
-  //      result += matches + ' '
-  //    }
-  //  } while (matches)
-
-  //  if (result) {
-  //    if (cmd.indexOf('node ') !== 0) {
-  //      return `electron_node ${result}`
-  //    }
-  //  }
-  //  return cmd
-  //}
-
   const CMD = '/bin/ps -ax -o pid=,ppid=,pcpu=,pmem=,command='
   const PID_CMD = /^\s*([0-9]+)\s+([0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+(.+)$/
 
@@ -133,17 +82,68 @@ const listProcesses = (rootPid: number): Promise<ProcessItem> => new Promise(don
   })
 })
 
+const findName = (cmd: string): string => {
+  const SHARED_PROCESS_HINT = /--disable-blink-features=Auxclick/
+  const WINDOWS_WATCHER_HINT = /\\watcher\\win32\\CodeHelper\.exe/
+  const WINDOWS_CRASH_REPORTER = /--crashes-directory/
+  const WINDOWS_PTY = /\\pipe\\winpty-control/
+  const WINDOWS_CONSOLE_HOST = /conhost\.exe/
+  const TYPE = /--type=([a-zA-Z-]+)/
+
+  // find windows file watcher
+  if (WINDOWS_WATCHER_HINT.exec(cmd)) return 'watcherService '
+
+  // find windows crash reporter
+  if (WINDOWS_CRASH_REPORTER.exec(cmd)) return 'electron-crash-reporter'
+
+  // find windows pty process
+  if (WINDOWS_PTY.exec(cmd)) return 'winpty-process'
+
+  //find windows console host process
+  if (WINDOWS_CONSOLE_HOST.exec(cmd)) return 'console-window-host (Windows internal process)'
+
+  // find "--type=xxxx"
+  let matches = TYPE.exec(cmd)
+  if (matches && matches.length === 2) {
+    if (matches[1] === 'renderer') {
+      if (SHARED_PROCESS_HINT.exec(cmd)) {
+        return 'shared-process'
+      }
+
+      return `window`
+    }
+    return matches[1]
+  }
+
+  // find all xxxx.js
+  const JS = /[a-zA-Z-]+\.js/g
+  let result = ''
+  do {
+    matches = JS.exec(cmd)
+    if (matches) {
+      result += matches + ' '
+    }
+  } while (matches)
+
+    if (result) {
+      if (cmd.indexOf('node ') !== 0) {
+        return `electron_node ${result}`
+      }
+    }
+  return cmd
+}
+
 const parseName = (cmd: string, pid: number): string => {
   // browser windows (renderer processes)
   if (pid === remote.process.pid) return 'Veonim'
   if (pid === process.pid) return 'Process Explorer'
 
   // neovim
-  if (cmd.includes('nvim') && cmd.includes('--cmd call rpcnotify')) return 'Neovim'
+  if (cmd.includes('nvim') && cmd.includes('call rpcnotify')) return 'Neovim'
   if (cmd.includes('nvim') && cmd.includes('--cmd colorscheme veonim')) return 'Neovim - Auxillary Syntax Highlighter'
   if (cmd.includes('nvim') && cmd.includes('--cmd com! -nargs=+ -range Veonim 1')) return 'Neovim - "errorformat" Parser'
 
-  return cmd
+  return findName(cmd)
 }
 
 const objToItem = ({ cmd, pid, load, memory, children = [] }: ProcessItem, list: Process[], depth = 0) => {
@@ -152,7 +152,7 @@ const objToItem = ({ cmd, pid, load, memory, children = [] }: ProcessItem, list:
     : (totalmem() * (memory / 100))
 
   const item: Process = {
-    cmd: ' '.repeat(depth * 2) + parseName(cmd, pid),
+    cmd: '&nbsp;'.repeat(depth * 4) + parseName(cmd, pid),
     cpu: Number(load.toFixed(0)),
     pid: Number((pid).toFixed(0)),
     memory: Number((mem / MB).toFixed(0)),
