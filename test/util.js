5154abaf4e8f9e8ccd2e48f7de180f399610479d
breja <gh@breja.io>
2018-09-18 20:57:12 -0400

    support global module proxying

--- test/util.js
+++ test/util.js
@@ -2,6 +2,8 @@
 
 const { deepStrictEqual: same } = require('assert')
 const proxyquire = require('proxyquire').noCallThru()
+const Module = require('module')
+const originalModuleLoader = Module._load
 
 const relativeFakes = obj => Object.keys(obj).reduce((res, key) => {
   const val = Reflect.get(obj, key)
@@ -13,4 +15,12 @@ const src = (name, fake, { noRelativeFake = false } = {}) => fake
   ? proxyquire(`../build/${name}`, noRelativeFake ? fake : relativeFakes(fake))
   : require(`../build/${name}`)
 
-module.exports = { src, same }
+const globalProxy = (name, implementation) => {
+  Module._load = (request, ...args) => request === name
+    ? implementation
+    : originalModuleLoader(request, ...args)
+
+  return () => Module._load = originalModuleLoader
+}
+
+module.exports = { src, same, globalProxy }
