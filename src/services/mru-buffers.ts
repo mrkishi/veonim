cc7884e36920f4e91f5e3ccd65962b6bd76f711c
breja <gh@breja.io>
2018-09-20 18:27:50 -0400

    jumping between mru buffers works

--- src/services/mru-buffers.ts
+++ src/services/mru-buffers.ts
@@ -1,7 +1,15 @@
 import nvim from '../core/neovim'
 
-const bufferStacks = new Map<number, string[]>()
+interface BufferStack {
+  /** offset from right (last item in stack). tracks position in the stack as
+   * we are jumping thru buffers */
+  jumpOffset: number
+  stack: string[]
+}
+
+const bufferStacks = new Map<number, BufferStack>()
 let activeWindow = 0
+let jumpingBufferStack = false
 
 // TODO: how do we cleanup windows that no longer exist?
 // ultimately i think we should tie this into render events
@@ -9,33 +17,70 @@ let activeWindow = 0
 // can track bufferName history directly. when the window gets
 // removed, so does the history for it. yeah i like that. lets do that
 
-nvim.onAction('buffer-next', async () => {
-  if (!bufferStacks.has(activeWindow)) return console.error('no buffer stacks - window does not exist. this should never happen')
-
-  const stack = bufferStacks.get(activeWindow)!
-  console.log('stack', stack)
-})
-
 nvim.onAction('buffer-prev', async () => {
   if (!bufferStacks.has(activeWindow)) return console.error('no buffer stacks - window does not exist. this should never happen')
 
-  const stack = bufferStacks.get(activeWindow)!
-  console.log('stack', stack)
+  const bs = bufferStacks.get(activeWindow)!
+
+  if (!bs.stack.length) return
+  if (bs.jumpOffset >= bs.stack.length - 1) return
+
+  bs.jumpOffset += 1
+  jumpingBufferStack = true
+
+  const endIndex = bs.stack.length - bs.jumpOffset - 1
+  const jumpBuffer = bs.stack[endIndex]
+
+  const bufferList = await nvim.buffers.list()
+  const buffers = await Promise.all(bufferList.map(async b => ({
+    id: b.id,
+    name: await b.name,
+  })))
+
+  const targetBuffer = buffers.find(b => b.name === jumpBuffer)
+  if (targetBuffer) nvim.cmd(`b ${targetBuffer.id}`) 
+})
+
+nvim.onAction('buffer-next', async () => {
+  if (!bufferStacks.has(activeWindow)) return console.error('no buffer stacks - window does not exist. this should never happen')
+
+  const bs = bufferStacks.get(activeWindow)!
+
+  if (!bs.stack.length) return
+  if (bs.jumpOffset < 1) return
+
+  bs.jumpOffset -= 1
+  jumpingBufferStack = true
+
+  const endIndex = bs.stack.length - bs.jumpOffset - 1
+  const jumpBuffer = bs.stack[endIndex]
+
+  const bufferList = await nvim.buffers.list()
+  const buffers = await Promise.all(bufferList.map(async b => ({
+    id: b.id,
+    name: await b.name,
+  })))
+
+  const targetBuffer = buffers.find(b => b.name === jumpBuffer)
+  if (targetBuffer) nvim.cmd(`b ${targetBuffer.id}`) 
 })
 
 nvim.on.winEnter(async id => {
   activeWindow = id
   const bufferName = await nvim.current.buffer.name
 
-  if (!bufferStacks.has(id)) return bufferStacks.set(id, [ bufferName ])
+  if (!bufferStacks.has(id)) return bufferStacks.set(id, {
+    jumpOffset: 0,
+    stack: [ bufferName ],
+  })
 
-  const stack = bufferStacks.get(id)!
+  const { stack, jumpOffset } = bufferStacks.get(id)!
   const lastItem = stack[stack.length - 1]
   if (lastItem !== bufferName) stack.push(bufferName)
 
   if (stack.length > 100) {
     const reducedStack = stack.slice(stack.length - 100)
-    bufferStacks.set(id, reducedStack)
+    bufferStacks.set(id, { jumpOffset, stack: reducedStack })
   }
 })
 
@@ -43,18 +88,35 @@ nvim.on.bufLoad(async () => {
   if (!bufferStacks.has(activeWindow)) return console.error('can not add buffer to stack - no window present. this is not supposed to happen')
 
   const bufferName = await nvim.current.buffer.name
-  const stack = bufferStacks.get(activeWindow)!
+  const { stack, jumpOffset } = bufferStacks.get(activeWindow)!
   const lastItem = stack[stack.length - 1]
+
+  if (jumpingBufferStack) {
+    jumpingBufferStack = false
+    return
+  }
+
+  if (jumpOffset) {
+    const endIndex = stack.length - jumpOffset - 1
+    const remainBufferStack = stack.slice(0, endIndex)
+    bufferStacks.set(activeWindow, { jumpOffset: 0, stack: remainBufferStack })
+    return
+  }
+
   if (lastItem !== bufferName) stack.push(bufferName)
 
   if (stack.length > 100) {
     const reducedStack = stack.slice(stack.length - 100)
-    bufferStacks.set(id, reducedStack)
+    bufferStacks.set(activeWindow, { jumpOffset, stack: reducedStack })
   }
 })
 
 setTimeout(async () => {
   activeWindow = await nvim.current.window.id
   const currentBufferName = await nvim.current.buffer.name
-  bufferStacks.set(activeWindow, [ currentBufferName ])
+
+  bufferStacks.set(activeWindow, {
+    jumpOffset: 0,
+    stack: currentBufferName ? [ currentBufferName ] : [],
+  })
 }, 1e3)
