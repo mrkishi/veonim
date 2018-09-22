13f366616f16b8385ef345adcc4647d185eb3358
breja <gh@breja.io>
2018-09-21 21:24:47 -0400

    hook placeholder vscode api

--- src/vscode/api.ts
+++ src/vscode/api.ts
@@ -1,5 +1,11 @@
+import extensions from '../vscode/extensions'
+import languages from '../vscode/languages'
+import workspace from '../vscode/workspace'
+import commands from '../vscode/commands'
+import window from '../vscode/window'
+import debug from '../vscode/debug'
+import tasks from '../vscode/tasks'
+import scm from '../vscode/scm'
 import env from '../vscode/env'
 
-export default {
-  env
-}
+export default { extensions, languages, workspace, commands, window, debug, tasks, scm, env }
