c3eb2551f65a97490c058026c1b644fb59bd9dcb
breja <gh@breja.io>
2018-09-23 15:00:42 -0400

    first api module is tested and working yay

--- test/integration/vscode-api-env.test.js
+++ test/integration/vscode-api-env.test.js
@@ -1,13 +1,28 @@
 const { src, same } = require('../util')
 
-const api = src('vscode/api')
+const { env } = src('vscode/api').default
 
 describe('vscode api - env', () => {
   describe('var', () => {
-    it('appName')
-    it('appRoot')
-    it('language')
-    it('machineId')
-    it('sessionId')
+    it('appName', () => {
+      same(env.appName, 'Veonim')
+    })
+
+    it('appRoot', () => {
+      same(env.appRoot, process.cwd())
+    })
+
+    it('language', () => {
+      same(env.language, 'en-US')
+    })
+
+    it('machineId', () => {
+      same(env.machineId, require('os').hostname())
+    })
+
+    it('sessionId', () => {
+      const includesName = env.sessionId.includes('Veonim-')
+      same(includesName, true, 'sessionId starts with Veonim-')
+    })
   })
 })
