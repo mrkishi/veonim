e955fe0bed9fe11bc2ea8ae9203d085ad8d7f94f
breja <gh@breja.io>
2018-09-23 14:54:35 -0400

    placeholder api workspace tests

---
+++ test/integration/vscode-api-workspace.test.js
@@ -0,0 +1,39 @@
+const { src, same } = require('../util')
+
+const api = src('vscode/api')
+
+describe('vscode api - workspace', () => {
+  describe('var', () => {
+    it('rootPath')
+    it('workspaceFolders')
+    it('name')
+    it('textDocuments')
+  })
+
+  describe('events', () => {
+    it('onWillSaveTextDocument')
+    it('onDidChangeWorkspaceFolders')
+    it('onDidOpenTextDocument')
+    it('onDidCloseTextDocument')
+    it('onDidChangeTextDocument')
+    it('onDidSaveTextDocument')
+    it('onDidChangeConfiguration')
+  })
+
+  describe('func', () => {
+    it('getWorkspaceFolder')
+    it('asRelativePath')
+    it('updateWorkspaceFolders')
+    it('createFileSystemWatcher')
+    it('findFiles')
+    it('saveAll')
+    it('applyEdit')
+    it('openTextDocument')
+    it('openTextDocument')
+    it('openTextDocument')
+    it('registerTextDocumentContentProvider')
+    it('getConfiguration')
+    it('registerTaskProvider')
+    it('registerFileSystemProvider')
+  })
+})
