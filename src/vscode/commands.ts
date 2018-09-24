56d1c4fb25c08920a14c9469bbf0b09e792687be
breja <gh@breja.io>
2018-09-24 08:48:45 -0400

    remove logging + add types to commands

--- src/vscode/commands.ts
+++ src/vscode/commands.ts
@@ -1,30 +1,23 @@
 import { EventEmitter } from 'events'
-import trace from '../support/trace'
 import * as vsc from 'vscode'
-
-const log = trace('vscodeapi')
 
 const registeredCommands = new EventEmitter()
 
 const commands: typeof vsc.commands = {
-  executeCommand: (command, ...args) => {
-    log('executeCommand:', command, args)
+  executeCommand: (command: string, ...args: any[]) => {
     registeredCommands.emit(command, ...args)
     return new Promise(() => {})
   },
-  getCommands: async (filterInternal) => {
-    log('getCommands:', filterInternal)
+  getCommands: async (filterInternal?: boolean) => {
     const events = registeredCommands.eventNames().map(m => m.toString())
     if (filterInternal) return events.filter(e => !e.startsWith('_'))
     return events
   },
-  registerCommand: (command, callback) => {
-    log('registerCommand', command, callback)
+  registerCommand: (command: string, callback: (...args: any[]) => void) => {
     registeredCommands.on(command, callback)
     return { dispose: () => registeredCommands.removeListener(command, callback) }
   },
-  registerTextEditorCommand: (command, callback) => {
-    log('registerTextEditorCommand', command, callback)
+  registerTextEditorCommand: (command: string, callback: Function) => {
     // TODO: the callback needs to return a TextEditor object. where do we get that from?
     // TODO: are we supposed to return textEditorCommands in getCommands or executeCommand?
     console.warn('NYI: registerTextEditorCommand', command, callback)
