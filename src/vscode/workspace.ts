95f0da52b9c2eeca5eba5f90952dfa8c28c84805
breja <gh@breja.io>
2018-09-24 16:51:06 -0400

    begin filling out the workspace api

--- src/vscode/workspace.ts
+++ src/vscode/workspace.ts
@@ -1,1 +1,19 @@
-export default {}
+import nvim from '../vscode/neovim'
+import { basename } from 'path'
+import * as vsc from 'vscode'
+
+const workspace: typeof vsc.workspace = {
+  get rootPath() { return nvim.state.cwd },
+  get workspaceFolders() { return [ nvim.state.cwd ] },
+  get name() { return basename(nvim.state.cwd) },
+  // TODO: NYI
+  // TODO: how to make this sync?
+  get textDocuments() {
+    console.warn('NYI: vscode.workspace.textDocuments')
+    return []
+  },
+  // TODO: events...
+  // TODO: functions...
+}
+
+export default workspace
