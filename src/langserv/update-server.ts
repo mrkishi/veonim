2478b39db3b1027624a6ad4ed0a4b6376cb0ddc4
breja <gh@breja.io>
2018-10-02 17:04:51 -0400

    update-server now switches between TDM and FDM depending on langserv

--- src/langserv/update-server.ts
+++ src/langserv/update-server.ts
@@ -1,6 +1,7 @@
 import { ProtocolConnection, DidOpenTextDocumentParams, DidChangeTextDocumentParams, WillSaveTextDocumentParams, DidSaveTextDocumentParams, DidCloseTextDocumentParams, TextDocumentSyncKind } from 'vscode-languageserver-protocol'
 import { vscLanguageToFiletypes } from '../langserv/vsc-languages'
 import TextDocumentManager from '../neovim/text-document-manager'
+import FullDocumentManager from '../neovim/full-document-manager'
 import { traceLANGSERV as log } from '../support/trace'
 import nvim from '../vscode/neovim'
 
@@ -10,11 +11,13 @@ interface LanguageServer extends ProtocolConnection {
   pauseTextSync: boolean
 }
 
-const incrementalUpdater = (server: LanguageServer, languageId: string) => {
+const updater = (server: LanguageServer, languageId: string, incremental = true) => {
   const limitedFiletypes = vscLanguageToFiletypes(languageId)
-  const tdm = TextDocumentManager(nvim, limitedFiletypes)
   let initialized = false
   let buffer: any[] = []
+  const m = incremental
+    ? TextDocumentManager(nvim, limitedFiletypes)
+    : FullDocumentManager(nvim, limitedFiletypes)
 
   server.untilInitialized.then(() => {
     buffer.forEach(([ method, params ]) => {
@@ -31,7 +34,7 @@ const incrementalUpdater = (server: LanguageServer, languageId: string) => {
     log(`NOTIFY --> textDocument/${method}`, params)
   }
 
-  tdm.on.didOpen(({ uri, version, languageId, textLines }) => send('didOpen', {
+  m.on.didOpen(({ uri, version, languageId, textLines }) => send('didOpen', {
     textDocument: {
       uri,
       version,
@@ -40,10 +43,7 @@ const incrementalUpdater = (server: LanguageServer, languageId: string) => {
     },
   } as DidOpenTextDocumentParams))
 
-  // TODO: how to handle servers that do not accept incremental updates?
-  // buffer whole file in memory and apply patches on our end? or query
-  // from filesystem and apply changes?
-  tdm.on.didChange(({ uri, version, textChanges }) => !server.pauseTextSync && send('didChange', {
+  m.on.didChange(({ uri, version, textChanges }) => !server.pauseTextSync && send('didChange', {
     textDocument: {
       uri,
       version,
@@ -54,40 +54,34 @@ const incrementalUpdater = (server: LanguageServer, languageId: string) => {
     }],
   } as DidChangeTextDocumentParams))
 
-  tdm.on.willSave(({ uri }) => send('willSave', {
+  m.on.willSave(({ uri }) => send('willSave', {
     reason: 1,
     textDocument: { uri },
   } as WillSaveTextDocumentParams))
 
-  tdm.on.didSave(({ uri, version }) => send('didSave', {
+  m.on.didSave(({ uri, version }) => send('didSave', {
     textDocument: {
       uri,
       version,
     },
   } as DidSaveTextDocumentParams))
 
-  tdm.on.didClose(({ uri }) => send('didClose', {
+  m.on.didClose(({ uri }) => send('didClose', {
     textDocument: { uri },
   } as DidCloseTextDocumentParams))
 
-  return { dispose: () => tdm.dispose() }
-}
-
-const fullUpdater = (server: LanguageServer, languageId: string) => {
-  console.warn(`Warning: Language server for ${languageId} does not support incremental text synchronization. This means a negative performance impact - especially on large buffers`)
-
-  // TODO: do the needful
-  // should we just bundle this functionality with TDM?
-  return { dispose: () => {} }
-}
-
-const noneUpdater = (languageId: string) => {
-  console.warn(`Warning: Language server for ${languageId} does not support any kind of text synchronization. This seems strange to me, but hey, maybe it works anyways.`)
-  return { dispose: () => {} }
+  return { dispose: () => m.dispose() }
 }
 
 export default (server: LanguageServer, languageId: string) => {
-  if (server.textSyncKind === TextDocumentSyncKind.Incremental) return  incrementalUpdater(server, languageId)
-  if (server.textSyncKind === TextDocumentSyncKind.Full) return fullUpdater(server, languageId)
-  return noneUpdater(languageId)
+  if (server.textSyncKind === TextDocumentSyncKind.Incremental) {
+    return updater(server, languageId, true)
+  }
+
+  if (server.textSyncKind === TextDocumentSyncKind.Full) {
+    console.warn(`Warning: Language server for ${languageId} does not support any kind of text synchronization. This seems strange to me, but hey, maybe it works anyways.`)
+    return updater(server, languageId, false)
+  }
+
+  return { dispose: () => {} }
 }
