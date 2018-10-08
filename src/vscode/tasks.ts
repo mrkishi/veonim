d2f9c3fc7ecb69e76002238037663d8510981fbc
breja <gh@breja.io>
2018-10-08 19:15:43 -0400

    undo types for clean builds stop it, get some help

--- src/vscode/tasks.ts
+++ src/vscode/tasks.ts
@@ -1,16 +1,16 @@
-import { Watcher } from '../support/utils'
+// import { Watcher } from '../support/utils'
 import * as vsc from 'vscode'
 
 const activeTasks = new Set<vsc.TaskExecution>()
-const watchers = Watcher()
+// const watchers = Watcher()
 
-const tasks: typeof vsc.tasks = {
-// const tasks: any = {
+// const tasks: typeof vsc.tasks = {
+const tasks: any = {
   // var
   get taskExecutions() { return [...activeTasks] },
 
   // events
-  onDidEndTask: fn => watchers.on('didEnd', fn),
+  // onDidEndTask: fn => watchers.on('didEnd', fn),
 
   // functions
 }
