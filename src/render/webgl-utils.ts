05ca3b1fa49068d7cc66c46c86781d92842dddc9
breja <gh@breja.io>
2018-10-09 16:59:22 -0400

    create shader helper util func

--- src/render/webgl-utils.ts
+++ src/render/webgl-utils.ts
@@ -2,6 +2,22 @@ export const WebGL2 = () => {
   const canvas = document.createElement('canvas')
   const gl = canvas.getContext('webgl2') as WebGLRenderingContext
 
+  const createShader = (type: number, source: string) => {
+    const shader = gl.createShader(type)
+
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+    if (success) return shader
+
+    console.log(gl.getShaderInfoLog(shader))
+    gl.deleteShader(shader)
+  }
+
+  const createVertexShader = (source: string) => createShader(gl.VERTEX_SHADER, source)
+  const createFragmentShader = (source: string) => createShader(gl.FRAGMENT_SHADER, source)
+
   const createProgram = (vertexShader: WebGLShader, fragmentShader: WebGLShader) => {
     const program = gl.createProgram()
 
@@ -16,5 +32,5 @@ export const WebGL2 = () => {
     gl.deleteProgram(program)
   }
 
-  return { createProgram }
+  return { createVertexShader, createFragmentShader, createProgram }
 }
