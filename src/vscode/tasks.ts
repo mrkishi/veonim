a87994c965f3866c3adef177990cd8f9d4c54545
breja <gh@breja.io>
2018-10-08 21:07:00 -0400

    vscodeapiover9000: register task event handlers

--- src/vscode/tasks.ts
+++ src/vscode/tasks.ts
@@ -1,16 +1,27 @@
-// import { Watcher } from '../support/utils'
+import { TaskStartEvent, TaskEndEvent, TaskProcessStartEvent, TaskProcessEndEvent } from 'vscode'
+import { Watcher } from '../support/utils'
 import * as vsc from 'vscode'
 
-const activeTasks = new Set<vsc.TaskExecution>()
-// const watchers = Watcher()
+interface Events {
+  didStartTask: TaskStartEvent
+  didEndTask: TaskEndEvent
+  didStartTaskProcess: TaskProcessStartEvent
+  didEndTaskProcess: TaskProcessEndEvent
+}
 
-// const tasks: typeof vsc.tasks = {
-const tasks: any = {
+const activeTasks = new Set<vsc.TaskExecution>()
+const watchers = Watcher<Events>()
+
+const tasks: typeof vsc.tasks = {
+// const tasks: any = {
   // var
   get taskExecutions() { return [...activeTasks] },
 
   // events
-  // onDidEndTask: fn => watchers.on('didEnd', fn),
+  onDidStartTask: fn => ({ dispose: watchers.on('didStartTask', fn) }),
+  onDidEndTask: fn => ({ dispose: watchers.on('didEndTask', fn) }),
+  onDidStartTaskProcess: fn => ({ dispose: watchers.on('didStartTaskProcess', fn) }),
+  onDidEndTaskProcess: fn => ({ dispose: watchers.on('didEndTaskProcess', fn) }),
 
   // functions
 }
