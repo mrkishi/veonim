df88b4dc5d1c646be58154245efb01c745adb657
breja <gh@breja.io>
2018-10-08 21:28:39 -0400

    more bullshit tasks

--- src/vscode/tasks.ts
+++ src/vscode/tasks.ts
@@ -1,29 +1,50 @@
-import { TaskStartEvent, TaskEndEvent, TaskProcessStartEvent, TaskProcessEndEvent } from 'vscode'
 import { Watcher } from '../support/utils'
 import * as vsc from 'vscode'
 
 interface Events {
-  didStartTask: TaskStartEvent
-  didEndTask: TaskEndEvent
-  didStartTaskProcess: TaskProcessStartEvent
-  didEndTaskProcess: TaskProcessEndEvent
+  didStartTask: vsc.TaskStartEvent
+  didEndTask: vsc.TaskEndEvent
+  didStartTaskProcess: vsc.TaskProcessStartEvent
+  didEndTaskProcess: vsc.TaskProcessEndEvent
+}
+
+interface MetaTask extends vsc.Task {
+  type: string
 }
 
 const activeTasks = new Set<vsc.TaskExecution>()
+const registeredTasks = new Set<MetaTask>()
 const watchers = Watcher<Events>()
 
+const provideTasksAndRegister = async (type: string, provider: vsc.TaskProvider) => {
+  const providedTasks = (await provider.provideTasks()) || []
+  providedTasks.forEach(task => registeredTasks.add(Object.assign(task, { type })))
+}
+
 const tasks: typeof vsc.tasks = {
-// const tasks: any = {
-  // var
   get taskExecutions() { return [...activeTasks] },
 
-  // events
   onDidStartTask: fn => ({ dispose: watchers.on('didStartTask', fn) }),
   onDidEndTask: fn => ({ dispose: watchers.on('didEndTask', fn) }),
   onDidStartTaskProcess: fn => ({ dispose: watchers.on('didStartTaskProcess', fn) }),
   onDidEndTaskProcess: fn => ({ dispose: watchers.on('didEndTaskProcess', fn) }),
 
-  // functions
+  registerTaskProvider: (type, provider) => {
+    provideTasksAndRegister(type, provider)
+    // TODO: when do we resolve a task?
+    return { dispose: () => console.warn('NYI: remove registered task provider') }
+  },
+
+  fetchTasks: async (filter = {}) => {
+    const filterQuery = filter.type
+    // TODO: get tasks from tasks.json (this will be an async op)
+    const tasksFromJson: MetaTask[] = []
+    return [...registeredTasks, ...tasksFromJson].filter(mt => mt.type === filterQuery)
+  },
+
+  executeTask: async task => {
+
+  },
 }
 
 export default tasks
