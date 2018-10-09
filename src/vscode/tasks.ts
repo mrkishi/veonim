89caa067fa61ed5ca9a65032e7e434adf1b866e6
breja <gh@breja.io>
2018-10-08 22:05:15 -0400

    uh lolwut

--- src/vscode/tasks.ts
+++ src/vscode/tasks.ts
@@ -30,6 +30,7 @@ const runTask = (task: vsc.Task): vsc.TaskExecution => {
 
   // TODO: actually start the task
   // start the task process and shit
+  // emit taskstartprocess and taskendprocess events
 
   const taskExec = { task, terminate }
   activeTasks.add(taskExec)
