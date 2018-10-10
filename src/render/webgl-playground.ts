c4702975d16f62bd13ecb4bbd4a72aa4c1eebbdf
breja <gh@breja.io>
2018-10-10 08:45:16 -0400

    playground mode

--- src/render/webgl-playground.ts
+++ src/render/webgl-playground.ts
@@ -3,12 +3,14 @@ import * as fontTextureAtlas from '../render/font-texture-atlas'
 const container = document.createElement('div')
 
 Object.assign(container.style, {
+  top: '50px',
   position: 'absolute',
   width: '100%',
   height: '100%',
 })
 
 const { element } = fontTextureAtlas.generateStandardSet()
+element.style.border = '1px solid yellow'
 container.appendChild(element)
 
 document.body.appendChild(container)
