8ca4ed1bef84d55c001991444ad61fdeebf82089
breja <gh@breja.io>
2018-10-12 22:37:46 -0400

    this is how you do textures probably

--- src/render/webgl-utils.ts
+++ src/render/webgl-utils.ts
@@ -102,12 +102,14 @@ export const WebGL2 = () => {
     return createProgramWithShaders(vshader, fshader)
   }
 
-  const setupCanvasTexture = (canvas: HTMLCanvasElement) => {
+  const setupCanvasTexture = (canvas: HTMLCanvasElement, textureUnit = gl.TEXTURE0) => {
+    gl.activeTexture(textureUnit)
     gl.bindTexture(gl.TEXTURE_2D, gl.createTexture())
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas)
-    gl.generateMipmap(gl.TEXTURE_2D)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas)
   }
 
   const setupArrayBuffer = (data: Float32Array) => {
