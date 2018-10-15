8091f54c08b86ad0962836c6c8f5121be2e98b87
breja <gh@breja.io>
2018-10-15 16:08:32 -0400

    resolution now different between texture and canvas

--- src/render/webgl-playground.ts
+++ src/render/webgl-playground.ts
@@ -46,15 +46,16 @@ const dothewebglthing = (canvasElement: HTMLCanvasElement) => {
   const vertexShader = `
     in vec2 a_position;
     in vec2 a_texCoord;
-    uniform vec2 u_resolution;
+    uniform vec2 u_canvas_res;
+    uniform vec2 u_texture_res;
 
     out vec2 v_texCoord;
 
     void main() {
-      vec2 zeroToOne = a_position / u_resolution;
+      vec2 zeroToOne = a_position / u_canvas_res;
       vec2 zeroToTwo = zeroToOne * 2.0;
       vec2 clipSpace = zeroToTwo - 1.0;
-      v_texCoord = a_texCoord / u_resolution;
+      v_texCoord = a_texCoord / u_texture_res;
       gl_Position = vec4(clipSpace, 0, 1);
     }
   `
@@ -79,8 +80,9 @@ const dothewebglthing = (canvasElement: HTMLCanvasElement) => {
   const loc = {
     a_position: gl.getAttribLocation(program, 'a_position'),
     a_texCoord: gl.getAttribLocation(program, 'a_texCoord'),
+    u_canvas_res: gl.getUniformLocation(program, 'u_canvas_res'),
+    u_texture_res: gl.getUniformLocation(program, 'u_texture_res'),
     u_color: gl.getUniformLocation(program, 'u_color'),
-    u_resolution: gl.getUniformLocation(program, 'u_resolution'),
     u_image: gl.getUniformLocation(program, 'u_image'),
   }
 
@@ -88,12 +90,13 @@ const dothewebglthing = (canvasElement: HTMLCanvasElement) => {
 
   // TODO: TODO TODO TODO TODO LOL TODO
   // support rendering on multiple lines
-  // why is it fuzzy - still fucked up if no alpha. help pls
-  //    THE SIZE OF SOMETHING IS NOT GOOD MAN
   // how much shit can we move to shaders for calc
   // change the arrays to not be float32
-  // yo can we combine the arrays for position + texture into one?
   // support char color thanks
+  // use vertexAttribDivisor to run the vertex position attributes
+  // more than once (instanced drawing with drawArraysInstanced)
+  // see cheat codes here: https://stackoverflow.com/a/46068799
+  // yo can we combine the arrays for position + texture into one?
 
   const goasijoibbb = positionVerticiezeisfaerffrrzz(cc.cell.width, cc.cell.height)
   const qqqqbrbr = boqibcszzxpp(goasijoibbb)
@@ -101,9 +104,9 @@ const dothewebglthing = (canvasElement: HTMLCanvasElement) => {
   const gimmeCookie = (char: string) => goasijoibbb(char.charCodeAt(0) - 32)
   const mindedMusicSessions = (phrase: string) => phrase.split('').reduce((res, m) => {
     return [...res, ...gimmeCookie(m)]
-  }, [])
+  }, [] as number[])
 
-  const urMomInsult = 'hello WORLD!!'
+  const urMomInsult = 'whatever you mean'
   tester.innerText = urMomInsult
   const poo = mindedMusicSessions(urMomInsult)
 
@@ -121,24 +124,24 @@ const dothewebglthing = (canvasElement: HTMLCanvasElement) => {
   setupVertexArray(loc.a_position, { size: 2, type: gl.FLOAT })
 
   setupCanvasTexture(canvasElement)
-  const thingSize = {
-    width: cc.cell.width * 50,
-    height: cc.cell.height * 1,
+
+  const res = {
+    canvas: {
+      width: cc.cell.width * 50,
+      height: cc.cell.height * 1,
+    },
+    texture: {
+      width: Math.round(canvasElement.width / 2),
+      height: Math.round(canvasElement.height / 2),
+    },
   }
 
-  resize(canvasElement.width / 2, canvasElement.height / 2)
-  console.log('cwh', gl.canvas.width, gl.canvas.height)
-  console.log('twh', canvasElement.width, canvasElement.height)
-  // resize(thingSize.width, thingSize.height)
+  resize(res.canvas.width, res.canvas.height)
   gl.useProgram(program)
   gl.uniform4fv(loc.u_color, new Float32Array([1.0, 0.86, 0.0, 1.0]))
   gl.uniform1i(loc.u_image, 0)
-  // TODO: we need to set a resolution of the TEXTURE SIZE
-  // and we need another resolution of the webgl canvas size
-  // TWO DIFFERENT!!!
-  // gl.uniform2f(loc.u_resolution, thingSize.width, thingSize.height)
-  gl.uniform2f(loc.u_resolution, canvasElement.width /2, canvasElement.height /2)
-  // gl.uniform2f(loc.u_resolution, gl.canvas.width, gl.canvas.height)
+  gl.uniform2f(loc.u_canvas_res, res.canvas.width, res.canvas.height)
+  gl.uniform2f(loc.u_texture_res, res.texture.width, res.texture.height)
   // gl.clearColor(0.0, 0.1, 0.1, 1.0)
   // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
   gl.drawArrays(gl.TRIANGLES, 0, urMomInsult.length * 6)
