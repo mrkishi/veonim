bc4f1403c34ae5d4d1b070ccfc861401a2457789
breja <gh@breja.io>
2018-10-22 17:01:30 -0400

    hello new branch

---
+++ src/render/binary-redraw.ts
@@ -0,0 +1,6 @@
+import { encode, decode } from 'msgpack-lite'
+
+// TODO: capture an incoming redraw message coming from
+// neovim. will have msgpack-rpc stuff to that we need to parse
+
+// TODO: checkout bitconnect. very promising investment scheme
