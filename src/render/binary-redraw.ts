8cde1d36e3b7faa5134a3573b19913403389d458
breja <gh@breja.io>
2018-10-23 09:01:03 -0400

    idk lern to net be dumb

--- src/render/binary-redraw.ts
+++ src/render/binary-redraw.ts
@@ -9,17 +9,93 @@ const typ = (m: any) => {
   if (m >= 0xa0 && m <= 0xbf) return { kind: 'fixstr', length: m - 0xa0 }
 }
 
+
 export default (data: any) => {
   const raw = data
   const parsed = decode(raw)
   const hex = Array.from(raw).map((buf: any) => buf.toString(16).padStart(2, '0'))
+  let ix = 0
+
+  const toStr = (raw: any, start: number, length: number) => {
+    console.log('toStr:', start, length)
+    const end = start + length
+    const str = raw.toString('utf8', start, end)
+    return [ end, str ]
+  }
+
+  const toArr = (raw: any, start: number, length: number) => {
+    console.log('toArr:', start, length)
+    let it = 0
+    let ix = start
+    const arr = []
+
+    while (it < length) {
+      const wut = typ(raw[ix])
+      console.log('wut', wut)
+
+      if (wut.kind === '+fixint') {
+        arr.push(wut.val)
+        ix++
+        it++
+      }
+
+      else if (wut.kind === 'fixarr') {
+        const [ nextIx, stuff ] = toArr(raw, ix, wut.length)
+        ix = nextIx
+        arr.push(stuff)
+        it++
+      }
+
+      else if (wut.kind === 'fixstr') {
+        const [ nextIx, stuff ] = toStr(raw, ix, wut.length)
+        ix = nextIx
+        arr.push(stuff)
+        it++
+      }
+
+      else it++
+    }
+
+    return [ ix, arr ]
+  }
 
   if (parsed[1] !== 'redraw') return
 
-  const kind = typ(raw[0])
+  console.log('---------------')
+
+  const { kind, length, val } = typ(raw[0])
+  let res
+  if (kind === 'fixarr') res = toArr(raw, 1, length)
+  console.log('res:', res)
+
+  // while (ix < 5) {
+  //   const kind = typ(raw[ix])
+
+
+  //   ix++
+  // }
+
   console.log('kind', kind)
 
   console.log('raw:', raw)
   console.log('hex:', hex)
   console.log('parsed:', parsed)
+  console.log('---------------')
 }
+
+/*
+[
+ 2,
+ redraw,
+ [
+   [grid_clear, [ ...args ]]
+   [grid_line,
+    [ args ]
+    [ args ]
+    [ args ]
+    [ args ]
+    [ args ]
+   ]
+ ]
+]
+ */
