309c234fade46bfba6daa54e6f8c9818521cc16d
breja <gh@breja.io>
2018-10-24 16:45:25 -0400

    msgpack: use tuples for types - maybe faster?

--- src/render/binary-redraw.ts
+++ src/render/binary-redraw.ts
@@ -1,8 +1,7 @@
+// SPEC: https://github.com/msgpack/msgpack/blob/master/spec.md
 import { decode } from 'msgpack-lite'
 
-// SPEC: https://github.com/msgpack/msgpack/blob/master/spec.md
-
-enum MPKind {
+enum MPK {
   Val,
   Arr,
   Map,
@@ -10,145 +9,165 @@ enum MPKind {
   Unknown,
 }
 
-interface TypKind {
-  val?: any
-  kind: MPKind
-  length: number
-  start: number
-}
-
-const typ = (raw: any, ix: number): TypKind => {
+// return type --> [MPK, start, length, value?]
+const typ = (raw: Buffer, ix: number) => {
   const m = raw[ix]
-  const def = { kind: MPKind.Val, start: ix, length: 1 }
-  const unknown =  { kind: MPKind.Unknown, start: ix, length: 0 }
-  if (m == 0xc0) return { ...def, val: null }
-  if (m == 0xc2) return { ...def, val: false }
-  if (m == 0xc3) return { ...def, val: true }
+
+  if (m == 0xc0) return [
+    MPK.Val,
+    ix,
+    1,
+    null,
+  ]
+
+  if (m == 0xc2) return [
+    MPK.Val,
+    ix,
+    1,
+    false,
+  ]
+
+  if (m == 0xc3) return [
+    MPK.Val,
+    ix,
+    1,
+    true,
+  ]
 
   // fixint
-  if (m >= 0x00 && m <= 0x7f) return { ...def, val: m - 0x00 }
+  if (m >= 0x00 && m <= 0x7f) return [
+    MPK.Val,
+    ix,
+    1,
+    m - 0x00,
+  ]
 
   // negative fixint
-  if (m >= 0xe0 && m <= 0xff) return { ...def, val: (m - 0x100) }
+  if (m >= 0xe0 && m <= 0xff) return [
+    MPK.Val,
+    ix,
+    1,
+    m - 0x100,
+  ]
 
   // uint8
-  if (m == 0xcc) return {
-    kind: MPKind.Val,
-    val: raw[ix + 1],
-    start: ix + 1,
-    length: 1,
-  }
+  if (m == 0xcc) return [
+    MPK.Val,
+    ix + 1,
+    1,
+    raw[ix + 1],
+  ]
 
   // int8
   if (m == 0xd0) {
     const val = raw[ix + 1]
-    return {
-      kind: MPKind.Val,
-      val: [val & 0x80] ? val - 0x100 : val,
-      start: ix + 1,
-      length: 1,
-    }
+    return [
+      MPK.Val,
+      ix + 1,
+      1,
+      (val & 0x80) ? val - 0x100 : val,
+    ]
   }
 
   // uint16
-  if (m == 0xcd) return {
-    kind: MPKind.Val,
-    val: (raw[ix + 1] << 8) + raw[ix + 2],
-    start: ix + 1,
-    length: 2,
-  }
+  if (m == 0xcd) return [
+    MPK.Val,
+    ix + 1,
+    2,
+    (raw[ix + 1] << 8) + raw[ix + 2],
+  ]
 
   // int16
   if (m === 0xd1) {
     const val = (raw[ix + 1] << 8) + raw[ix + 2]
-    return {
-      kind: MPKind.Val,
-      val: (val & 0x8000) ? val - 0x10000 : val,
-      start: ix + 1,
-      length: 2,
-    }
+    return [
+      MPK.Val,
+      ix + 1,
+      2,
+      (val & 0x8000) ? val - 0x10000 : val,
+    ]
   }
 
   // uint32
-  if (m == 0xce) return {
-    kind: MPKind.Val,
-    val: (raw[ix + 1] * 16777216) + (raw[ix + 2] << 16) + (raw[ix + 3] << 8) + raw[ix + 4],
-    start: ix + 1,
-    length: 4,
-  }
+  if (m == 0xce) return [
+    MPK.Val,
+    ix + 1,
+    4,
+    (raw[ix + 1] * 16777216) + (raw[ix + 2] << 16) + (raw[ix + 3] << 8) + raw[ix + 4],
+  ]
 
   // int32
-  if (m == 0xd2) return {
-    kind: MPKind.Val,
-    val: (raw[ix + 1] << 24) | (raw[ix + 2] << 16) | (raw[ix + 3] << 8) | raw[ix + 4],
-    start: ix + 1,
-    length: 4,
-  }
+  if (m == 0xd2) return [
+    MPK.Val,
+    ix + 1,
+    4,
+    (raw[ix + 1] << 24) | (raw[ix + 2] << 16) | (raw[ix + 3] << 8) | raw[ix + 4],
+  ]
+
+  // fixarr
+  if (m >= 0x90 && m <= 0x9f) return [
+    MPK.Arr,
+    ix + 1,
+    m - 0x90,
+  ]
+
+  // fixmap
+  if (m >= 0x80 && m <= 0x8f) return [
+    MPK.Map,
+    ix + 1,
+    m - 0x80,
+  ]
+
+  // fixstr
+  if (m >= 0xa0 && m <= 0xbf) return [
+    MPK.Str,
+    ix + 1,
+    m - 0xa0,
+  ]
+
+  // arr16
+  if (m == 0xdc) return [
+    MPK.Arr,
+    ix + 3,
+    raw[ix + 1] + raw[ix + 2],
+  ]
+
+  // arr32
+  if (m == 0xdd) return [
+    MPK.Arr,
+    ix + 5,
+    raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
+  ]
+
+  // map16
+  if (m == 0xde) return [
+    MPK.Map,
+    ix + 3,
+    raw[ix + 1] + raw[ix + 2],
+  ]
+
+  // map32
+  if (m == 0xdf) return [
+    MPK.Map,
+    ix + 5,
+    raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
+  ]
 
   // uint64
   if (m == 0xcf) {
     console.warn('uint64 not supported')
-    return unknown
+    return [MPK.Unknown, ix, 0]
   }
 
   // int64
   if (m == 0xd3) {
     console.warn('int64 not supported')
-    return unknown
-  }
-
-  // fixarr
-  if (m >= 0x90 && m <= 0x9f) return {
-    kind: MPKind.Arr,
-    length: m - 0x90,
-    start: ix + 1,
-  }
-
-  // fixmap
-  if (m >= 0x80 && m <= 0x8f) return {
-    kind: MPKind.Map,
-    length: m - 0x80,
-    start: ix + 1,
-  }
-
-  // fixstr
-  if (m >= 0xa0 && m <= 0xbf) return {
-    kind: MPKind.Str,
-    length: m - 0xa0,
-    start: ix + 1,
-  }
-
-  // arr16
-  if (m == 0xdc) return {
-    kind: MPKind.Arr,
-    length: raw[ix + 1] + raw[ix + 2],
-    start: ix + 3,
-  }
-
-  // arr32
-  if (m == 0xdd) return {
-    kind: MPKind.Arr,
-    length: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
-    start: ix + 5,
-  }
-
-  // map16
-  if (m == 0xde) return {
-    kind: MPKind.Map,
-    length: raw[ix + 1] + raw[ix + 2],
-    start: 3,
-  }
-
-  // map32
-  if (m == 0xdf) return {
-    kind: MPKind.Map,
-    length: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
-    start: 5,
+    return [MPK.Unknown, ix, 0]
   }
 
   const byte = m.toString(16).padStart(2, '0')
-  console.warn('not sure how to parse:', byte, def.start)
-  return { kind: MPKind.Unknown, start: ix, length: 0 }
+  console.warn('not sure how to parse:', byte, ix)
+  return [MPK.Unknown, ix, 0]
 }
 
 type ParseResult = [ number, any ]
@@ -193,17 +212,15 @@ const toArr = (raw: any, start: number, length: number): ParseResult => {
   return [ ix, res ]
 }
 
-const parse = (raw: Buffer, { val, kind, start, length }: TypKind): ParseResult => {
-  if (val !== undefined) return [ start + length, val ]
-  if (kind === MPKind.Arr) return toArr(raw, start, length)
-  if (kind === MPKind.Str) return toStr(raw, start, length)
-  if (kind === MPKind.Map) return toMap(raw, start, length)
+const parse = (raw: Buffer, [ kind, start, length, val ]: any[]): ParseResult => {
+  if (kind === MPK.Val) return [ start + length, val ]
+  if (kind === MPK.Arr) return toArr(raw, start, length)
+  if (kind === MPK.Str) return toStr(raw, start, length)
+  if (kind === MPK.Map) return toMap(raw, start, length)
   return [ start + length, undefined ]
 }
 
 export default (raw: any) => {
-
-
   console.log('---------------')
   console.time('msgpack')
   const parsed = decode(raw)
