a489bd87feef8b210353596e0b36f264ec53a256
breja <gh@breja.io>
2018-10-24 08:53:00 -0400

    redo parse setup and types kinds

--- src/render/binary-redraw.ts
+++ src/render/binary-redraw.ts
@@ -3,36 +3,61 @@ import { decode } from 'msgpack-lite'
 // SPEC: https://github.com/msgpack/msgpack/blob/master/spec.md
 
 enum MPKind {
+  Val,
   Arr,
   Map,
   Str,
 }
 
-const typ = (raw: any, ix: number) => {
+interface TypKind {
+  val?: any
+  kind: MPKind
+  length: number
+  start: number
+}
+
+const typ = (raw: any, ix: number): TypKind => {
   const m = raw[ix]
-  if (m == 0xc0) return { val: null }
-  if (m == 0xc2) return { val: false }
-  if (m == 0xc3) return { val: true }
+  const def = { kind: MPKind.Val, start: ix, length: 1 }
+  if (m == 0xc0) return { ...def, val: null }
+  if (m == 0xc2) return { ...def, val: false }
+  if (m == 0xc3) return { ...def, val: true }
 
   // fixint
-  if (m >= 0x00 && m <= 0x7f) return { val: m - 0x00 }
+  if (m >= 0x00 && m <= 0x7f) return { ...def, val: m - 0x00 }
 
   // TODO: verify how we parse unsigned ints??
   // uint8
-  if (m == 0xcc) return { val: raw[ix + 1] }
+  if (m == 0xcc) return {
+    kind: MPKind.Val,
+    val: raw[ix + 1],
+    start: ix + 1,
+    length: 1,
+  }
 
   // uint16
-  if (m == 0xcd) return { val: raw[ix + 1] + raw[ix + 2] }
+  if (m == 0xcd) return {
+    kind: MPKind.Val,
+    val: raw[ix + 1] + raw[ix + 2],
+    start: ix + 1,
+    length: 2,
+  }
 
   // uint32
   if (m == 0xce) return {
+    kind: MPKind.Val,
     val: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
+    start: ix + 1,
+    length: 4,
   }
 
   // uint64
   if (m == 0xcf) return {
+    kind: MPKind.Val,
     val: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4]
        + raw[ix + 5] + raw[ix + 6] + raw[ix + 7] + raw[ix + 8],
+    start: ix + 1,
+    length: 8,
   }
 
   // fixarr
@@ -84,7 +109,8 @@ const typ = (raw: any, ix: number) => {
     start: 5,
   }
 
-  return { kind: m.toString(16).padStart(2, '0'), length: 0 }
+  // TODO: only for dev.
+  return { ...def, kind: m.toString(16).padStart(2, '0') }
 }
 
 type ParseResult = [ number, any ]
@@ -95,12 +121,67 @@ const toMap = (raw: any, start: number, length: number): ParseResult => {
   const res = {}
 
   while (ix < length) {
-    const wut = typ(raw, ix)
+    const keywut = typ(raw, ix)
+    const valwut = typ(raw, ix + 1)
     console.log('OBJ! wut', wut)
-    ix++
+    ix += 2
   }
 
   return [ ix, res ]
+}
+
+const toStr = (raw: any, start: number, length: number): ParseResult => {
+  const end = start + length
+  const str = raw.toString('utf8', start, end)
+  return [ end, str ]
+}
+
+const toArr = (raw: any, start: number, length: number): ParseResult => {
+  let it = 0
+  let ix = start
+  const arr = []
+
+  while (it < length) {
+    const wut = typ(raw, ix)
+
+    if (wut.val) {
+      arr.push(wut.val)
+      ix++
+      it++
+    }
+
+    else if (wut.kind === MPKind.Arr) {
+      const [ nextIx, stuff ] = toArr(raw, wut.start, wut.length)
+      ix = nextIx
+      arr.push(stuff)
+      it++
+    }
+
+    else if (wut.kind === MPKind.Str) {
+      const [ nextIx, stuff ] = toStr(raw, wut.start, wut.length)
+      ix = nextIx
+      arr.push(stuff)
+      it++
+    }
+
+    else if (wut.kind === MPKind.Map) {
+      const [ nextIx, stuff ] = toMap(raw, wut.start, wut.length)
+      ix = nextIx
+      arr.push(stuff)
+      it++
+    }
+
+    else {
+      console.warn('no idea how to handle element:', wut.kind)
+      it++
+    }
+  }
+
+  return [ ix, arr ]
+}
+
+const parse = ({ val, kind, start, length }: TypKind): ParseResult => {
+  // if (val) return [ start,  ]
 }
 
 export default (data: any) => {
@@ -109,55 +190,7 @@ export default (data: any) => {
   const hex = Array.from(raw).map((buf: any) => buf.toString(16).padStart(2, '0'))
   let ix = 0
 
-  const toStr = (raw: any, start: number, length: number): ParseResult => {
-    const end = start + length
-    const str = raw.toString('utf8', start, end)
-    return [ end, str ]
-  }
 
-  const toArr = (raw: any, start: number, length: number): ParseResult => {
-    let it = 0
-    let ix = start
-    const arr = []
-
-    while (it < length) {
-      const wut = typ(raw, ix)
-
-      if (wut.val) {
-        arr.push(wut.val)
-        ix++
-        it++
-      }
-
-      else if (wut.kind === MPKind.Arr) {
-        const [ nextIx, stuff ] = toArr(raw, wut.start, wut.length)
-        ix = nextIx
-        arr.push(stuff)
-        it++
-      }
-
-      else if (wut.kind === MPKind.Str) {
-        const [ nextIx, stuff ] = toStr(raw, wut.start, wut.length)
-        ix = nextIx
-        arr.push(stuff)
-        it++
-      }
-
-      else if (wut.kind === MPKind.Map) {
-        const [ nextIx, stuff ] = toMap(raw, wut.start, wut.length)
-        ix = nextIx
-        arr.push(stuff)
-        it++
-      }
-
-      else {
-        console.warn('no idea how to handle element:', wut.kind)
-        it++
-      }
-    }
-
-    return [ ix, arr ]
-  }
 
   if (parsed[1] !== 'redraw') return
 
