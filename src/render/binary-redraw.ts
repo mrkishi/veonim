2656ac3edd13d283cde3aab8e96de4ece9d04177
breja <gh@breja.io>
2018-10-24 08:28:22 -0400

    parse use enums for better perf ya

--- src/render/binary-redraw.ts
+++ src/render/binary-redraw.ts
@@ -2,6 +2,12 @@ import { decode } from 'msgpack-lite'
 
 // SPEC: https://github.com/msgpack/msgpack/blob/master/spec.md
 
+enum MPKind {
+  Arr,
+  Map,
+  Str,
+}
+
 const typ = (raw: any, ix: number) => {
   const m = raw[ix]
   if (m == 0xc0) return { val: null }
@@ -9,55 +15,54 @@ const typ = (raw: any, ix: number) => {
   if (m == 0xc3) return { val: true }
 
   if (m >= 0x00 && m <= 0x7f) return {
-    kind: '+fixint',
     val: m - 0x00,
   }
 
   // fixarr
   if (m >= 0x90 && m <= 0x9f) return {
-    kind: 'arr',
+    kind: MPKind.Arr,
     length: m - 0x90,
     start: ix + 1,
   }
 
   // fixmap
   if (m >= 0x80 && m <= 0x8f) return {
-    kind: 'map',
+    kind: MPKind.Map,
     length: m - 0x80,
     start: ix + 1,
   }
 
   // fixstr
   if (m >= 0xa0 && m <= 0xbf) return {
-    kind: 'str',
+    kind: MPKind.Str,
     length: m - 0xa0,
     start: ix + 1,
   }
 
   // arr16
   if (m == 0xdc) return {
-    kind: 'arr',
+    kind: MPKind.Arr,
     length: raw[ix + 1] + raw[ix + 2],
     start: ix + 3,
   }
 
   // arr32
   if (m == 0xdd) return {
-    kind: 'arr',
+    kind: MPKind.Arr,
     length: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
     start: ix + 5,
   }
 
   // map16
   if (m == 0xde) return {
-    kind: 'map',
+    kind: MPKind.Map,
     length: raw[ix + 1] + raw[ix + 2],
     start: 3,
   }
 
   // map32
   if (m == 0xdf) return {
-    kind: 'map',
+    kind: MPKind.Map,
     length: raw[ix + 1] + raw[ix + 2] + raw[ix + 3] + raw[ix + 4],
     start: 5,
   }
@@ -93,14 +98,14 @@ export default (data: any) => {
         it++
       }
 
-      else if (wut.kind === 'arr') {
+      else if (wut.kind === MPKind.Arr) {
         const [ nextIx, stuff ] = toArr(raw, wut.start, wut.length)
         ix = nextIx
         arr.push(stuff)
         it++
       }
 
-      else if (wut.kind === 'str') {
+      else if (wut.kind === MPKind.Str) {
         const [ nextIx, stuff ] = toStr(raw, wut.start, wut.length)
         ix = nextIx
         arr.push(stuff)
@@ -123,7 +128,7 @@ export default (data: any) => {
   const { kind, length, start } = typ(raw, 0)
   console.log('init:', kind, start, length)
   let res
-  if (kind === 'arr') res = toArr(raw, start, length)
+  if (kind === MPKind.Arr) res = toArr(raw, start, length)
 
   console.log('parsed:', parsed)
   console.log('res:', res[1])
